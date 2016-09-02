0898ac4c48cdace1526ab537cacc16c0ba2e7f27
Jason Tan <jason.tan@vanderbilt.edu>
2016-09-02 18:25:45 -0500

    Add ability to create MayoLINK order

--- src/Pmi/Mayolink/Order.php
+++ src/Pmi/Mayolink/Order.php
@@ -43,4 +43,36 @@ class Order
             return false;
         }
     }
+
+    public function create($options)
+    {
+        $body = [
+            'authenticity_token' => $this->csrfToken,
+            'order[test_requests_attributes][0][test_code]' => $options['test_code'],
+            "temperatures[{$options['test_code']}][{$options['specimen']}]" => $options['temperature'],
+            'order[patient_attributes][first_name]' => $options['first_name'],
+            'order[patient_attributes][last_name]' => $options['last_name'],
+            'order[patient_attributes][gender]' => $options['gender'],
+            'order[patient_attributes][birth_date]' => $options['birth_date']->format('Y-m-d'),
+            'order[physician_name]' => $options['physician_name'],
+            'order[physician_phone]' => $options['physician_phone'],
+            'order[collected_at(1i)]' => $options['collected_at']->format('Y'),
+            'order[collected_at(2i)]' => $options['collected_at']->format('n'),
+            'order[collected_at(3i)]' => $options['collected_at']->format('j'),
+            'order[collected_at(4i)]' => $options['collected_at']->format('H'),
+            'order[collected_at(5i)]' => $options['collected_at']->format('i')
+        ];
+        $response = $this->client->request('POST', "{$this->ordersEndpoint}/en/orders", [
+            'form_params' => $body,
+            'allow_redirects' => false
+        ]);
+        if ($response->getStatusCode() !== 302 || empty($response->getHeader('Location'))) {
+            return false;
+        }
+        $location = $response->getHeader('Location')[0];
+        if (!preg_match('/orders\/(.*)$/', $location, $matches)) {
+            return false;
+        }
+        return $matches[1];
+    }
 }
--- tests/Pmi/Mayolink/OrderTest.php
+++ tests/Pmi/Mayolink/OrderTest.php
@@ -3,11 +3,33 @@ use Pmi\Mayolink\Order;
 
 class OrderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSearch()
+    public function testLogin()
     {
         $order = new Order();
         // TODO: retrieve test credentials from... datastore?
         $loginResult = $order->login('username', 'password');
         $this->assertTrue($loginResult, 'Failed to authenticate with MayoLINK');
+        return $order;
+    }
+
+    /**
+     * @depends testLogin
+     */
+    public function testOrder($order)
+    {
+        $options = [
+            'test_code' => 'ACE',
+            'specimen' => 'Serum',
+            'temperature' => 'Ambient',
+            'first_name' => 'First',
+            'last_name' => 'Last',
+            'gender' => 'M',
+            'birth_date' => new \DateTime('1990-01-01'),
+            'physician_name' => 'None',
+            'physician_phone' => 'None',
+            'collected_at' => new \DateTime('-1 day')
+        ];
+        $orderResult = $order->create($options);
+        $this->assertNotEmpty($orderResult);
     }
 }
