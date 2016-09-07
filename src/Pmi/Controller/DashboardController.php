8a97eea8a293f8409e7c707a04811f7d045a164f
Jason Tan <jason.tan@vanderbilt.edu>
2016-09-07 15:00:12 -0500

    Add dashboard demo with SQL example

--- src/Pmi/Controller/DashboardController.php
+++ src/Pmi/Controller/DashboardController.php
@@ -9,11 +9,24 @@ class DashboardController extends AbstractController
     protected static $name = 'dashboard';
     
     protected static $routes = [
-        ['home', '/']
+        ['home', '/'],
+        ['demo', '/demo']
     ];
     
     public function homeAction(Application $app, Request $request)
     {
         return $app['twig']->render('dashboard/index.html.twig');
+    }
+
+    public function demoAction(Application $app, Request $request)
+    {
+        $rows = $app['db']->fetchAll('SELECT state, value FROM demo order by state');
+        $states = [];
+        foreach ($rows as $row) {
+            $states[$row['state']] = (int)$row['value'];
+        }
+        return $app['twig']->render('dashboard/demo.html.twig', [
+            'states' => $states
+        ]);
     }
 }
