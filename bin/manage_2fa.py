865d4850d5347208ab9e726d1eb94328a5b63d42
David Bernick <dbernick@broadinstitute.org>
2016-10-28 10:44:51 -0400

    Made report generation smoother

--- bin/manage_2fa.py
+++ bin/manage_2fa.py
@@ -9,7 +9,7 @@ from oauth2client.file import Storage
 from googleapiclient.discovery import build
 
 from oauth2client.service_account import ServiceAccountCredentials
-
+from datetime import date, timedelta
 #pip install httplib2 boto google-api-python-client
 
 try:
@@ -29,20 +29,11 @@ APPLICATION_NAME = 'Directory API Python Quickstart'
 GOOGLE_2FA_EXCEPTION_GROUP = 'mfa_exception@pmi-ops.org'
 
 def create_directory_service(user_email):
-    """Build and returns an Admin SDK Directory service object authorized with the service accounts
-    that act on behalf of the given user.
-
-    Args:
-      user_email: The email of the user. Needs permissions to access the Admin APIs.
-    Returns:
-      Admin SDK directory service object.
-    """
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name(CLIENT_SECRET_FILE, DIR_SCOPES)
     delegated_credentials = credentials.create_delegated(DELEGATE_ACCOUNT)
     http_auth = delegated_credentials.authorize(httplib2.Http())
 
-    #credentials = credentials.create_delegated(user_email)
 
     return build('admin', 'directory_v1', http=http_auth)
 
@@ -53,21 +44,36 @@ def create_report_service(user_email):
     delegated_credentials = credentials.create_delegated(DELEGATE_ACCOUNT)
     http_auth = delegated_credentials.authorize(httplib2.Http())
 
-    #del_credentials = credentials.create_delegated(user_email)
 
     return build('admin', 'reports_v1', http=http_auth)
+
+def getReportResults(useremail,daycount):
+    reportservice = create_report_service(SERVICE_ACCOUNT_USER)
+
+    results = None
+    while results is None:
+        try:
+            # connect
+            #
+            yesterday = date.today() - timedelta(days=daycount)
+            results = reportservice.userUsageReport().get(userKey=useremail,date=yesterday.strftime('%Y-%m-%d'),parameters='accounts:is_2sv_enrolled').execute()
+        except:
+            daycount=daycount+1
+
+    return results,daycount
 
 def getNo2FAUsers(users,google_2fa_members):
     reportservice = create_report_service(SERVICE_ACCOUNT_USER)
     userlist=[]
     removeList=[]
+
     if not users:
         print('No users in the domain.')
     else:
         for user in users:
             userEmail = user['primaryEmail'].encode('utf-8')
-            results = reportservice.userUsageReport().get(userKey=user['primaryEmail'],date='2016-10-23',parameters='accounts:is_2sv_enrolled').execute()
-            #print(results.get('usageReports'))
+            daycount=1
+            results,daycount = getReportResults(userEmail,daycount)
             if results.get('usageReports'):
                 for item in results.get('usageReports'):
                     if item.get("parameters"):
@@ -75,12 +81,10 @@ def getNo2FAUsers(users,google_2fa_members):
                             #if user has not activated 2fa and is in 2fa_exception group, list
                             #if user has activated 2fa and is in 2fa exception group, remove from exception group
                             if not par.get("boolValue") and userEmail in google_2fa_members:
-                                #print('{0} ({1})'.format(userEmail,par.get("boolValue")))
                                 userlist.append(userEmail)
                             elif par.get("boolValue") and userEmail in google_2fa_members:
                                 removeList.append(userEmail)
             else:
-                #print('{0} ({1})'.format(userEmail,False))
                 userlist.append(userEmail)
     return userlist,removeList
 
@@ -94,6 +98,9 @@ def main():
     results = dirservice.users().list(domain = 'pmi-ops.org',orderBy='email').execute()
     users = results.get('users', [])
     no2faUsers,removeList = getNo2FAUsers(users,google_2fa_members)
+
+    print("users without 2fa: %s" % no2faUsers)
+    print("users to remove from 2fa exception: %s" % removeList)
 
     for user in removeList:
         print("Removing %s" % user)
