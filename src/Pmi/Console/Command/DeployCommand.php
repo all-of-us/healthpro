0f738a1c61bcac90de0d15f3d9d6b00ccf55a0a3
Ross Davis <ross.davis@vanderbilt.edu>
2016-11-09 10:25:37 -0600

    Don't display errors in test (only in local and dev)

--- src/Pmi/Console/Command/DeployCommand.php
+++ src/Pmi/Console/Command/DeployCommand.php
@@ -399,7 +399,7 @@ class DeployCommand extends Command {
             if (strlen($line) === 0) {
                 continue;
             } elseif (preg_match('/^display_errors\s*=/i', $line)) {
-                $line = 'display_errors = ' . ($this->isProd() ? 0 : 1);
+                $line = 'display_errors = ' . (($this->isProd() || $this->isTest()) ? 0 : 1);
             } elseif (preg_match('/^error_reporting\s*=/i', $line)) {
                 $line = 'error_reporting = ' . ($this->isProd() ?
                     'E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED' : 'E_ALL');
