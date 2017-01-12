93427eb6a215a7aacac9283ba280f93c739e88e6
Jonathan Franklin <jonathan.franklin@vanderbilt.edu>
2017-01-12 14:48:48 -0600

    Correct find/replace oopsies

--- sql/healthpro/sites.sql
+++ sql/healthpro/sites.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `sites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT),
-  `name` varchar(255) NOT NULL),
-  `google_group` varchar(255) NOT NULL),
-  `mayolink_account` varchar(255) NOT NULL),
-  `timezone` varchar(100) DEFAULT NULL),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `google_group` varchar(255) NOT NULL,
+  `mayolink_account` varchar(255) NOT NULL,
+  `timezone` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8mb4;
 
