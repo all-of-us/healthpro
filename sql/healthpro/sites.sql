0893288f6643f6ed389d09e4d9e69c099f186280
Jonathan Franklin <jonathan.franklin@vanderbilt.edu>
2017-01-12 14:47:28 -0600

    Add SQL to insert site data, guessing at site names

--- sql/healthpro/sites.sql
+++ sql/healthpro/sites.sql
@@ -1,12 +1,48 @@
 CREATE TABLE `sites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `google_group` varchar(255) NOT NULL,
-  `mayolink_account` varchar(255) NOT NULL,
-  `timezone` varchar(100) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT),
+  `name` varchar(255) NOT NULL),
+  `google_group` varchar(255) NOT NULL),
+  `mayolink_account` varchar(255) NOT NULL),
+  `timezone` varchar(100) DEFAULT NULL),
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `sites` (name, google_group, mayolink_account) VALUES ('Hogwarts', 'hogwarts-google-group', 'hogwarts-mayo');
-INSERT INTO `sites` (name, google_group, mayolink_account) VALUES ('Durmstrang', 'durmstrang-google-group','durmstrang-mayo');
-INSERT INTO `sites` (name, google_group, mayolink_account) VALUES ('Beauxbatons', 'beauxbatons-google-group','beauxbatons-mayo');
+INSERT INTO `sites` (name, google_group, mayolink_account) VALUES 
+('Hogwarts', 'hogwarts-google-group', 'hogwarts-mayo'),
+('Durmstrang', 'durmstrang-google-group','durmstrang-mayo'),
+('Beauxbatons', 'beauxbatons-google-group','beauxbatons-mayo'),
+('A', 'a', '7035588'),
+('B', 'b', '7035500'),
+('University of Arizona CATS','uofacats', '7035650'),
+('Banner Health Campus','bannerscampus', '7035651'),
+('Banner Health Phoenix','bannerphoenix', '7035652'),
+('Banner Health Estrella','bannerestrella', '7035653'),
+('Banner Health Desert','bannerdesert', '7035654'),
+('University of Illinois at Chicago Research','uicresearch', '7035707'),
+('Northwestern University Galter Health Sciences Library','nwfeinberggalter', '7035702'),
+('University of Chicago','uofchicago', '7035703'),
+('Rush University','rushuniv', '7035704'),
+('NorthShore University Health System','northshoree', '7035705'),
+('Columbia University Irving Institute','irvingcolumbia', '7035709'),
+('Harlem','harlem', '7035710'),
+('Weill Cornell Medical Center','weillcornell', '7035711'),
+('San Diego Blood Bank','sdbb', '7035735'),
+('Walgreens','walgreens', '7035735'),
+('VA Boston HC','vabostonhc', '7035759'),
+('VA Palo Alto','vapaloalto', '7035758'),
+('Jeanette Phillips','jeanettephillips', '7035760'),
+('Monroeville','monroeville', '7035769'),
+('Aiken Medical Center','aikenmed', '7035770'),
+('University of Pittsburgh Medical Center','upmc', '7035771'),
+('Montefiore CTRC','montefiorectrc', '7035772'),
+('Pelion Family Practice','pelionfp', '7035779'),
+('Waverly Family Practice','waverlyfp', '7035780'),
+('Eau Claire Wisconsin','eauclairewi', '7035781'),
+('Morristown','morristown', '7035777'),
+('Knoxville','knoxville', '7035778'),
+('James Anderson','jamesanderson', '7035782'),
+('Copiah Medical Associates','copiah', '7035783'),
+('Otay Mesa Medical Offices','otay', '7035784'),
+('Comprehensive Health Center Ocean View','chcoceanview', '7035785');
+
+# SDBB and Walgreens share the same account
