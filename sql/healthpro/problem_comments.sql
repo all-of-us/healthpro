8366d238b753b6b7ba38b1d6c0dd59c0dcfec024
Shyam Naru <shyam.s.naru@vanderbilt.edu>
2017-07-26 10:57:12 -0400

    Create problem comments db table

---
+++ sql/healthpro/problem_comments.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `problem_comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `problem_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `site` varchar(50) NOT NULL,
+  `staff_name` varchar(255) DEFAULT NULL,
+  `comment` text DEFAULT NULL,
+  `created_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8mb4;
