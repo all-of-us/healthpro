0c6c2bdae1bdb42a2ac146c3adcc4c0dc63c3315
Shyam Naru <shyam.s.naru@vanderbilt.edu>
2017-08-14 09:48:42 -0500

    Write bash script to execute sql queries in circle ci

--- ci/database.sh
+++ ci/database.sh
@@ -1,31 +1,8 @@
-mysql -u ubuntu -e "CREATE TABLE problems (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  user_id int(11) NOT NULL,
-  site varchar(50) NOT NULL,
-  participant_id varchar(50) NOT NULL,
-  problem_type varchar(50) DEFAULT NULL,
-  enrollment_site varchar(255) DEFAULT NULL,
-  staff_name varchar(255) DEFAULT NULL,
-  problem_date timestamp NULL DEFAULT NULL,
-  provider_aware_date timestamp NULL DEFAULT NULL,
-  description text DEFAULT NULL,
-  action_taken text DEFAULT NULL,
-  finalized_user_id int(11) DEFAULT NULL,
-  finalized_site varchar(50) DEFAULT NULL,
-  finalized_ts TIMESTAMP NULL DEFAULT NULL,
-  created_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8mb4;" circle_test
-mysql -u ubuntu -e "CREATE TABLE problem_comments (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  problem_id int(11) NOT NULL,
-  user_id int(11) NOT NULL,
-  site varchar(50) NOT NULL,
-  staff_nam varchar(255) DEFAULT NULL,
-  comment text DEFAULT NULL,
-  created_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  FOREIGN KEY (problem_id)
-  REFERENCES problems(id)
-) DEFAULT CHARSET=utf8mb4;" circle_test
+#!/bin/bash
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+FILES="${APP_DIR}/sql/healthpro/*"
+for FILE in $FILES
+do
+  mysql -u ubuntu circle_test < $FILE
+done
+
