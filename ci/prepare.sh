15113788b731eff0e472c1cff1835adb162b68bb
Jason Tan <jason.tan@vanderbilt.edu>
2017-08-17 15:16:32 -0500

    Patch php_cli.py to allow CI environment variables

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# install composer dependencies
 composer install
 
+# install App Engine SDK
 cd ~
 wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.57.zip
 unzip -q google_appengine_1.9.57.zip
+
+# patch php_cli.py to make our CI environment variables available
+patch google_appengine/google/appengine/tools/php_cli.py php_cli.patch
