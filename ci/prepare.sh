9e36a180a4f7b9f680ead3224efd3e426f163fae
Jason Tan <jason.tan@vanderbilt.edu>
2017-08-17 15:34:36 -0500

    Specify path to patch file

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # install composer dependencies
 composer install
 
@@ -9,4 +11,4 @@ wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9
 unzip -q google_appengine_1.9.57.zip
 
 # patch php_cli.py to make our CI environment variables available
-patch google_appengine/google/appengine/tools/php_cli.py php_cli.patch
+patch google_appengine/google/appengine/tools/php_cli.py $DIR/php_cli.patch
