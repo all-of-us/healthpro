faf21b3575d665eb1bfacee048a0cc31911d089e
Shyam Naru <shyam.s.naru@vanderbilt.edu>
2017-08-22 10:35:46 -0500

    Move report kit comfirm modal to app.js file

--- web/assets/js/views/CreateOrder.js
+++ web/assets/js/views/CreateOrder.js
@@ -45,24 +45,6 @@ $(document).ready(function() {
     if ($("#createOrder").length > 0) {
         new CreateOrder({el: $("#createOrder") });
     }
-    $('button.reportKit').click(function() {
-        var location = $(this).data('href');
-        var msg = 'You are leaving the DRC HealthPro web application and accessing a non-FISMA destination.';
-        msg += '<ul>';
-        msg += '<li>Entering of participant information is prohibited at the destination.</li>';
-        msg += '<li>This external link provides additional information that is consistent with the intended purpose of HealthPro. DRC cannot attest to the accuracy of a non-DRC site.</li>';
-        msg += '</ul>';
-        msg += 'Linking to a non-DRC site does not constitute endorsement by DRC or any of its employees of the sponsors or information and products presented on the site. You will be subject to the destination site\'s privacy policy when you follow the link.'
-        new PmiConfirmModal({
-            title: 'Attention',
-            msg: msg,
-            isHTML: true,
-            onTrue: function() {
-                window.open(location, '_blank');
-            },
-            btnTextTrue: 'Continue'
-        });
-    });
 });
 
 })(jQuery); // END wrapper
