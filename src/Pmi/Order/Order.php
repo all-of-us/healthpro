bc62ae0c52cdf1ca1d0e2ea8b03b144faa06cb0e
Shyam Naru <shyam.s.naru@vanderbilt.edu>
2017-10-23 12:59:30 -0500

    Auto select centrifuge type in order process form

--- src/Pmi/Order/Order.php
+++ src/Pmi/Order/Order.php
@@ -321,10 +321,16 @@ class Order
                 'required' => false
             ]);
             if ($this->order['type'] !== 'saliva' && !empty($enabledSamples)) {
+                $sites = $this->app['em']->getRepository('sites')->fetchOneBy([
+                    'google_group' => $this->app->getSiteId()
+                ]);
+                $centrifugeType = $sites ? $sites['centrifuge_type'] : null;
+                $centrifugeType = !empty($formData['processed_centrifuge_type']) ? $formData['processed_centrifuge_type'] : $centrifugeType;
                 $formBuilder->add('processed_centrifuge_type', Type\ChoiceType::class, [
                     'label' => 'Centrifuge type',
                     'required' => true,
                     'disabled' => $disabled,
+                    'data' => $centrifugeType,
                     'choices' => [
                         '-- Select centrifuge type --' => null,
                         'Fixed Angle'=> self::FIXED_ANGLE,
