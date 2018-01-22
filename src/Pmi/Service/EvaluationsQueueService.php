e3938f4546def2d84c4e77d9da40c8fa27783442
Shyam Naru <shyam.s.naru@vumc.org>
2018-01-22 10:58:14 -0600

    Update/Insert fhir version in evaluations/queue tables after resend

--- src/Pmi/Service/EvaluationsQueueService.php
+++ src/Pmi/Service/EvaluationsQueueService.php
@@ -5,6 +5,7 @@ use Pmi\Mail\Message;
 use Pmi\Audit\Log;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Pmi\Evaluation\Evaluation;
+use Pmi\Evaluation\Fhir;
 
 class EvaluationsQueueService
 {
@@ -35,12 +36,13 @@ class EvaluationsQueueService
             $fhir = $evaluationService->getFhir($evaluation['finalized_ts'], $queue['old_rdr_id']);
             if ($rdrEvalId = $this->rdr->createEvaluation($evaluation['participant_id'], $fhir)) {
                 $now = new \DateTime();
-                $this->em->getRepository('evaluations')->update($evaluation['id'], ['rdr_id' => $rdrEvalId]);
-                $this->em->getRepository('evaluations_queue')->update($queue['id'], ['new_rdr_id' => $rdrEvalId, 'sent_ts' => $now]);
+                $this->em->getRepository('evaluations')->update($evaluation['id'], ['rdr_id' => $rdrEvalId, 'fhir_version' => Fhir::CURRENT_VERSION]);
+                $this->em->getRepository('evaluations_queue')->update($queue['id'], ['new_rdr_id' => $rdrEvalId, 'fhir_version' => Fhir::CURRENT_VERSION, 'sent_ts' => $now]);
                 $this->app->log(Log::QUEUE_RESEND_EVALUATION, [
                 	'id' => $queue['id'],
                     'old_rdr_id' => $queue['old_rdr_id'],
-                    'new_rdr_id' => $rdrEvalId
+                    'new_rdr_id' => $rdrEvalId,
+                    'fhir_version' => Fhir::CURRENT_VERSION
                 ]);
             } else {
             	syslog(LOG_ERR, "#{$evalId} failed sending to RDR: " .$this->rdr->getLastError());
