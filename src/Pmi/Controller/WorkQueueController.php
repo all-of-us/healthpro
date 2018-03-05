860fa68a7b714c64ccfdde9fe0619a480fe4e584
Shyam Naru <shyam.s.naru@vumc.org>
2018-03-05 10:25:25 -0600

    Remove unused  variable

--- src/Pmi/Controller/WorkQueueController.php
+++ src/Pmi/Controller/WorkQueueController.php
@@ -219,7 +219,7 @@ class WorkQueueController extends AbstractController
         if (!empty($sites)) {
             $sitesList = [];
             $sitesList['site']['label'] = 'Paired Site Location';
-            foreach ($sites as $key => $site) {
+            foreach ($sites as $site) {
                 $sitesList['site']['options'][$site['google_group']] = \Pmi\Security\User::SITE_PREFIX.$site['google_group'];
             }
             $filters = array_merge($filters, $sitesList);
