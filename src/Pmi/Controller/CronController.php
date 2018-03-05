acffc010cb1437895169e8836e0db273db9a6e58
Jason Tan <jason.tan@vumc.org>
2018-03-05 17:30:11 -0600

    Add method to sync sites with Org API

--- src/Pmi/Controller/CronController.php
+++ src/Pmi/Controller/CronController.php
@@ -17,7 +17,8 @@ class CronController extends AbstractController
 
     protected static $routes = [
         ['pingTest', '/ping-test'],
-        ['withdrawal', '/withdrawal']
+        ['withdrawal', '/withdrawal'],
+        ['sites', '/sites']
     ];
     
     /**
@@ -57,6 +58,55 @@ class CronController extends AbstractController
             error_log('Cron ping test requested by Appengine-Cron');
         }
         
+        return (new JsonResponse())->setData(true);
+    }
+
+    public function sitesAction(Application $app, Request $request)
+    {
+        $rdrHelper = $app['pmi.drc.rdrhelper'];
+        $client = $rdrHelper->getClient();
+        $response = $client->request('GET', 'rdr/v1/Awardee');
+        $responseObject = json_decode($response->getBody()->getContents());
+        $siteRepository = $app['em']->getRepository('sites');
+        foreach ($responseObject->entry as $entry) {
+            $awardee = $entry->resource;
+            if (!isset($awardee->organizations) || !is_array($awardee->organizations)) {
+                break;
+            }
+            foreach ($awardee->organizations as $organization) {
+                if (!isset($organization->sites) || !is_array($organization->sites)) {
+                    break;
+                }
+                foreach ($organization->sites as $site) {
+                    $googleGroup = str_replace(\Pmi\Security\User::SITE_PREFIX, '', $site->id);
+                    $existing = $siteRepository->fetchOneBy([
+                        'google_group' => $googleGroup
+                    ]);
+                    if ($existing) {
+                        $siteData = $existing;
+                    } else {
+                        $siteData = [];
+                    }
+                    $siteData['name'] = $site->displayName;
+                    $siteData['google_group'] = $googleGroup;
+                    $siteData['mayolink_account'] = $site->mayolinkClientNumber;
+                    $siteData['timezone'] = $site->timeZoneId;
+                    $siteData['organization'] = $awardee->id;
+                    $siteData['type'] = $awardee->type;
+                    // $siteData['awardee'] = ?? not sure what this should be since organization is actually awardee. i think to be backwards-compatible, this might be for dv's only
+                    // $siteData['email'] = ?? data doesn't seem to be correct and is an array but not properly separated?
+
+                    if (empty($siteData['workqueue_download'])) {
+                        $siteData['workqueue_download'] = 'full_data';
+                    }
+                    if ($existing) {
+                        $siteRepository->update($existing['id'], $siteData);
+                    } else {
+                        $siteRepository->insert($siteData);
+                    }
+                }
+            }
+        }
         return (new JsonResponse())->setData(true);
     }
 }
