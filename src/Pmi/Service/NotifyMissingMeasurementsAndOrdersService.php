2093e98b9953a0136918ea5ae02bbe68b870db3d
Shyam Naru <shyam.s.naru@vumc.org>
2018-06-22 17:04:09 -0500

    Modify fetch missing orders query

--- src/Pmi/Service/NotifyMissingMeasurementsAndOrdersService.php
+++ src/Pmi/Service/NotifyMissingMeasurementsAndOrdersService.php
@@ -22,12 +22,12 @@ class NotifyMissingMeasurementsAndOrdersService
 
     public function sendEmails()
     {
-        $missingEvaluations = $this->db->fetchAll('select id from evaluations where id not in (select record_id from missing_notifications_log where type="' . self::MEASUREMENT_TYPE . '") and finalized_ts is not null and rdr_id is null');
+        $missingEvaluations = $this->db->fetchAll('select id from evaluations where id not in (select record_id from missing_notifications_log where type="' . self::MEASUREMENT_TYPE . '") and finalized_ts is not null and mayo_id is not null and rdr_id is null');
         foreach ($missingEvaluations as $evaluation) {
             $this->insertRecords($evaluation['id'], self::MEASUREMENT_TYPE);
         }
 
-        $missingOrders = $this->db->fetchAll('select id from orders where id not in (select record_id from missing_notifications_log where type="' . self::ORDER_TYPE . '") and finalized_ts is not null and rdr_id is null');
+        $missingOrders = $this->db->fetchAll('select id from orders where id not in (select record_id from missing_notifications_log where type="' . self::ORDER_TYPE . '") and finalized_ts is not null and mayo_id is not null and rdr_id is null');
         foreach ($missingOrders as $order) {
             $this->insertRecords($order['id'], self::ORDER_TYPE);
         }
