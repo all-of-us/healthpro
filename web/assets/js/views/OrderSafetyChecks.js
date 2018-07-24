7fef407756a04c6e3c2f5da7c2f1eb55b47818ce
Shyam Naru <shyam.s.naru@vumc.org>
2018-07-24 14:49:58 -0500

    Move the whole branching logic into a single function

--- web/assets/js/views/OrderSafetyChecks.js
+++ web/assets/js/views/OrderSafetyChecks.js
@@ -17,49 +17,62 @@ $(document).ready(function() {
         }
     };
 
-    $('input:radio[name=donate]').on('change', function() {
-        if ($(this).val() === 'yes') {
+    var applyBranchingLogic = function() {
+        var donate = $('input:radio[name=donate]:checked').val();
+        var isTransfusionChecked = $('input:radio[name=transfusion]:checked').val();
+        var isTransfusionWholeBloodChecked = $('input:checkbox[name=transfusion_whole_blood]').is(':checked');
+        var isTransfusionRedBloodChecked = $('input:checkbox[name=transfusion_red_blood]').is(':checked');
+        var isRedBloodQnChecked = $('input:radio[name=red_blood_qn]').is(':checked');
+        if (donate === 'yes') {
             hideFields(['transfusion', 'blood-info-text-2', 'saliva-info-text']);
             showFields(['order-info-text', 'blood-info-text', 'urine-info-text', 'continue']);
         } else {
             showFields(['transfusion']);
-            hideFields(['transfusion-qn', 'red-blood-qn', 'order-info-text', 'continue']);
-        }
-    });
-
-    $('input:radio[name=transfusion]').on('change', function() {
-        if ($(this).val() === 'yes') {
-            showFields(['transfusion-qn']);
-            hideFields(['continue']);
-        } else {
-            hideFields(['transfusion-qn', 'red-blood-qn', 'order-info-text']);
-            showFields(['continue']);
-        }
-    });
-
-    $('input:radio[name=red_blood_qn]').on('change', function() {
-        if ($(this).val() === 'yes') {
-            hideFields(['order-info-text']);
-            showFields(['continue']);
-        } else {
-            showFields(['order-info-text', 'blood-info-text-2', 'saliva-info-text', 'continue']);
-            hideFields(['blood-info-text', 'urine-info-text']);
-        }
-    });
-
-    $('input[type=checkbox]').on('change', function() {
-        var transfusionWholeBlood = $('input:checkbox[name=transfusion_whole_blood]:checked').val();
-        var transfusionRedBlood = $('input:checkbox[name=transfusion_red_blood]:checked').val();
-        if (typeof(transfusionWholeBlood) !== 'undefined' || typeof(transfusionRedBlood) !== 'undefined') {
-            if ((transfusionWholeBlood === 'whole') || (transfusionWholeBlood === 'whole' && transfusionRedBlood === 'red')) {
-                hideFields(['red-blood-qn', 'blood-info-text-2', 'saliva-info-text']);
-                showFields(['order-info-text', 'blood-info-text', 'urine-info-text', 'continue']);
-            } else {
-                showFields(['red-blood-qn']);
-                hideFields(['order-info-text', 'blood-info-text', 'blood-info-text-2', 'urine-info-text', 'continue']);
+            var hideFieldNames = ['order-info-text', 'continue'];
+            if (!isTransfusionWholeBloodChecked && !isTransfusionRedBloodChecked) {
+                hideFieldNames.push('transfusion-qn');
             }
-        } else {
-            hideFields(['red-blood-qn', 'order-info-text', 'continue']);
+            if (!isRedBloodQnChecked) {
+                hideFieldNames.push('red-blood-qn');
+            }
+            hideFields(hideFieldNames);
+            if (isTransfusionChecked) {
+                var transfusion = $('input:radio[name=transfusion]:checked').val();
+                if (transfusion === 'yes') {
+                    showFields(['transfusion-qn']);
+                    hideFields(['continue']);
+                    if (isTransfusionWholeBloodChecked || isTransfusionRedBloodChecked) {
+                        var transfusionWholeBlood = $('input:checkbox[name=transfusion_whole_blood]:checked').val();
+                        var transfusionRedBlood = $('input:checkbox[name=transfusion_red_blood]:checked').val();
+                        if ((transfusionWholeBlood === 'whole') || (transfusionWholeBlood === 'whole' && transfusionRedBlood === 'red')) {
+                            hideFields(['red-blood-qn', 'blood-info-text-2', 'saliva-info-text']);
+                            showFields(['order-info-text', 'blood-info-text', 'urine-info-text', 'continue']);
+                        } else {
+                            showFields(['red-blood-qn']);
+                            hideFields(['order-info-text', 'blood-info-text', 'blood-info-text-2', 'urine-info-text', 'continue']);
+                            if (isRedBloodQnChecked) {
+                                var redBloodQn = $('input:radio[name=red_blood_qn]:checked').val();
+                                if (redBloodQn === 'yes') {
+                                    hideFields(['order-info-text']);
+                                    showFields(['continue']);
+                                } else {
+                                    showFields(['order-info-text', 'blood-info-text-2', 'saliva-info-text', 'continue']);
+                                    hideFields(['blood-info-text', 'urine-info-text']);
+                                }
+                            }
+                        }
+                    } else {
+                        hideFields(['red-blood-qn', 'order-info-text', 'continue']);
+                    }
+                } else {
+                    hideFields(['transfusion-qn', 'red-blood-qn', 'order-info-text']);
+                    showFields(['continue']);
+                }
+            }
         }
+    };
+
+    $('input[type=radio], input[type=checkbox]').on('change', function() {
+        applyBranchingLogic();
     });
 });
