1d2a5321bb0b3ceb15a923637803dc372ac64c0a
Jason Tan <jason.tan@vumc.org>
2018-08-09 13:31:58 -0500

    Use https for geoip database download

--- src/Pmi/Console/Command/FirewallCommand.php
+++ src/Pmi/Console/Command/FirewallCommand.php
@@ -31,7 +31,7 @@ class FirewallCommand extends Command
         $output->setFormatter(new OutputFormatter(true)); // color output
         
         $output->writeln("Downloading GeoIP2 country database...");
-        $db = file_get_contents('http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz');
+        $db = file_get_contents('https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz');
         $dbFile = "{$this->appDir}/bin/GeoLite2-Country.mmdb";
         file_put_contents($dbFile, gzdecode($db));
         $output->writeln("... database downloaded to {$dbFile}");
