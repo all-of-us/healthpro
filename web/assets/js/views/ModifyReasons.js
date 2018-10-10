8848aab37672bcfeed85b643fece2b619f52f7c2
Shyam Naru <shyam.s.naru@vumc.org>
2018-10-10 11:03:47 -0500

    PM modify add cancel/restore reasons

---
+++ web/assets/js/views/ModifyReasons.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    // Ignore non-modify reasons pages.
+    if (!$('#form_reason').length) {
+        return;
+    }
+
+    $('#form_other_text').hide();
+
+    var selected = $( "#form_reason option:selected").val();
+    var other = $("#form_reason option:contains(Other)").val();
+    if (other === selected) {
+        $('#form_other_text').show();
+    }
+
+    $('#form_reason').on('change', function () {
+        if ($(this).val() === other) {
+            $('#form_other_text').show();
+        } else {
+            $('#form_other_text').hide();
+        }
+    })
+});
