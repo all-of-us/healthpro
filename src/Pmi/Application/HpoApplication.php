e8e00ec86a59000de45bd065c8776cedaa2414b4
Jason Tan <jason.tan@vumc.org>
2019-02-13 17:23:01 -0600

    Render full-page notices

--- src/Pmi/Application/HpoApplication.php
+++ src/Pmi/Application/HpoApplication.php
@@ -437,6 +437,13 @@ class HpoApplication extends AbstractApplication
 
         $noticeService = new NoticeService($this['em']);
         $notices = $noticeService->getCurrentNotices($request->getPathInfo());
+        foreach ($notices as $notice) {
+            if ($notice['full_page']) {
+                return new Response($this['twig']->render('full-page-notice.html.twig', [
+                    'message' => $notice['message']
+                ]));
+            }
+        }
         $app['twig']->addGlobal('global_notices', $notices);
     }
 
