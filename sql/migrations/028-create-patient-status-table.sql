551f06bf65c8c1d31e977d6318d7b0dc583b34c9
Shyam Naru <shyam.s.naru@vumc.org>
2019-04-23 11:36:43 -0500

    Combine two migrations

--- sql/migrations/028-create-patient-status-table.sql
+++ sql/migrations/028-create-patient-status-table.sql
@@ -7,3 +7,18 @@ CREATE TABLE `patient_status` (
   PRIMARY KEY (`id`),
   KEY `history_id` (`history_id`)
 ) DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `patient_status_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_status_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `site` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `comments` text DEFAULT NULL,
+  `created_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`patient_status_id`)
+  REFERENCES patient_status(`id`)
+) DEFAULT CHARSET=utf8mb4;
+
