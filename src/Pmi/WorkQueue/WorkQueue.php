4275cc97ad8c581a0283d5d48cd204c78ced0bdb
Shyam Naru <shyam.s.naru@vumc.org>
2019-04-30 16:48:31 -0500

    Do not display seconds

--- src/Pmi/WorkQueue/WorkQueue.php
+++ src/Pmi/WorkQueue/WorkQueue.php
@@ -356,7 +356,7 @@ class WorkQueue
             try {
                 $date = new \DateTime($string);
                 $date->setTimezone(new \DateTimeZone($timezone));
-                return $date->format('m/d/Y h:i:s a');
+                return $date->format('m/d/Y h:i a');
             } catch (\Exception $e) {
                 return '';
             }
