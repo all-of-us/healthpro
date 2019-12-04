c5bedc9b6712a65978529f23c1be604af7116c8f
Shyam Naru <shyam.s.naru@vumc.org>
2019-12-04 13:57:53 -0600

    Update local router to include csv and json urls

--- web/local-router.php
+++ web/local-router.php
@@ -2,10 +2,9 @@
 if (preg_match('#^/assets/#', $_SERVER["REQUEST_URI"])) {
     // always pass through for static assets
     return false;
-} elseif (preg_match('/\\.pdf$/', $_SERVER["REQUEST_URI"])) {
-    // for non-asset PDF URLs, explicitly send to index.php controller to avoid
+} elseif (preg_match('/\\.pdf|csv|json$/', $_SERVER["REQUEST_URI"])) {
+    // for non-asset PDF, CSV and JSON URLs, explicitly send to index.php controller to avoid
     // the dev server from assuming these are static assets
-    syslog(LOG_INFO, $_SERVER["REQUEST_URI"] . ' (dynamic PDF request)');
     require 'index.php';
 } else {
     // everything else can pass through, since index.php is the default controller
