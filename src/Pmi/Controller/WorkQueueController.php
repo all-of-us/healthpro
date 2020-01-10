2caa8c641fc4438da8fc95c5c1d08f14578f2b48
Shyam Naru <shyam.s.naru@vumc.org>
2020-01-10 13:40:56 -0600

    Dispaly in-person enrollement details

--- src/Pmi/Controller/WorkQueueController.php
+++ src/Pmi/Controller/WorkQueueController.php
@@ -8,6 +8,8 @@ use Pmi\Entities\Participant;
 use Pmi\Drc\CodeBook;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Pmi\WorkQueue\WorkQueue;
+use Pmi\Order\Order;
+use Pmi\Evaluation\Evaluation;
 
 class WorkQueueController extends AbstractController
 {
@@ -473,9 +475,21 @@ class WorkQueueController extends AbstractController
         if (!$participant) {
             $app->abort(404);
         }
+        $evaluationService = new Evaluation($app);
+        $evaluations = $evaluationService->getEvaluationsWithHistory($id);
+
+        $order = new Order($app);
+        $orders = $order->getParticipantOrdersWithHistory($id);
+
+        $query = "SELECT p.id, p.updated_ts, p.finalized_ts, MAX(pc.created_ts) as last_comment_ts, count(pc.comment) as comment_count FROM problems p LEFT JOIN problem_comments pc on p.id = pc.problem_id WHERE p.participant_id = ? GROUP BY p.id ORDER BY IFNULL(MAX(pc.created_ts), updated_ts) DESC";
+        $problems = $app['db']->fetchAll($query, [$id]);
+
         return $app['twig']->render('workqueue/participant.html.twig',[
             'participant' => $participant,
-            'cacheEnabled' => $app['pmi.drc.participants']->getCacheEnabled()
+            'cacheEnabled' => $app['pmi.drc.participants']->getCacheEnabled(),
+            'orders' => $orders,
+            'evaluations' => $evaluations,
+            'problems' => $problems
         ]);
     }
 
