6a2ba5b6c00d7dc5017ab6115c3f4955372b4e77
Shyam Naru <shyam.s.naru@vumc.org>
2020-02-19 14:28:39 -0600

    Check if token getUser method returns an object

--- src/Pmi/Application/AbstractApplication.php
+++ src/Pmi/Application/AbstractApplication.php
@@ -322,7 +322,10 @@ abstract class AbstractApplication extends Application
     public function getUser()
     {
         $token = $this['security.token_storage']->getToken();
-        return $token ? $token->getUser() : null;
+        if ($token) {
+            return is_object($token->getUser()) ? $token->getUser() : null;
+        }
+        return null;
     }
 
     public function getUserId()
