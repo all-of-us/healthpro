13fd3add0e4c635be2d918a1bf353e2bc4f1068c
Shyam Naru <shyam.s.naru@vumc.org>
2020-04-14 18:34:25 -0500

    Use single composer file and vendor directory

--- symfony/config/bootstrap.php
+++ symfony/config/bootstrap.php
@@ -2,7 +2,7 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__).'../../vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
