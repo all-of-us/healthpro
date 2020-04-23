b14ed080f0e587c2ad8ed340a175b439962c278e
Shyam Naru <shyam.s.naru@vumc.org>
2020-04-23 12:18:31 -0500

    Remove app entry point and redirect based on request uri

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$url = @parse_url($_SERVER['REQUEST_URI'])['path'];
+
+if (preg_match("/^\/s(\/|$)/", $url)) {
+    require 'symfony/public/index.php';
+} else {
+    require 'web/index.php';
+}
