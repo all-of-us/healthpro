f2acbc6bbefebd2c54b4883ae355077c45b8d32b
Shyam Naru <shyam.s.naru@vumc.org>
2020-06-17 17:47:38 -0500

    Check for ROLE_BIOBANK

--- src/Pmi/Controller/BiobankController.php
+++ src/Pmi/Controller/BiobankController.php
@@ -201,7 +201,7 @@ class BiobankController extends AbstractController
         $finalizeForm = $order->createBiobankOrderFinalizeForm($site);
         $finalizeForm->handleRequest($request);
         if ($finalizeForm->isSubmitted()) {
-            if ($order->isOrderDisabled() || $app->hasRole('ROLE_AWARDEE')) {
+            if ($order->isOrderDisabled() || !$app->hasRole('ROLE_BIOBANK')) {
                 $app->abort(403);
             }
             if (!$order->isOrderFormDisabled()) {
