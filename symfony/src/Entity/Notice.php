97112e85d9b918fa9b7fbf999e3c37c014a0aae2
Jason Tan <jason.tan@vumc.org>
2020-07-14 17:02:36 -0500

    Add Symfony support for page notices

---
+++ symfony/src/Entity/Notice.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NoticeRepository")
+ * @ORM\Table(name="notices")
+ */
+class Notice
+{
+    /**
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="url", type="string", length=255, nullable=false)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(name="message", type="text", length=65535, nullable=false)
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(name="full_page", type="boolean", nullable=false)
+     */
+    private $fullPage = false;
+
+    /**
+     * @ORM\Column(name="start_ts", type="datetime", nullable=true)
+     */
+    private $startTs;
+
+    /**
+     * @ORM\Column(name="end_ts", type="datetime", nullable=true)
+     */
+    private $endTs;
+
+    /**
+     * @ORM\Column(name="status", type="boolean", nullable=false)
+     */
+    private $status = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getFullPage(): ?bool
+    {
+        return $this->fullPage;
+    }
+
+    public function setFullPage(bool $fullPage): self
+    {
+        $this->fullPage = $fullPage;
+
+        return $this;
+    }
+
+    public function getStartTs(): ?\DateTimeInterface
+    {
+        return $this->startTs;
+    }
+
+    public function setStartTs(?\DateTimeInterface $startTs): self
+    {
+        $this->startTs = $startTs;
+
+        return $this;
+    }
+
+    public function getEndTs(): ?\DateTimeInterface
+    {
+        return $this->endTs;
+    }
+
+    public function setEndTs(?\DateTimeInterface $endTs): self
+    {
+        $this->endTs = $endTs;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
--- symfony/src/EventListener/RequestListener.php
+++ symfony/src/EventListener/RequestListener.php
@@ -2,16 +2,26 @@
 
 namespace App\EventListener;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Twig\Environment as TwigEnvironment;
+use App\Entity\Notice;
 use App\Service\LoggerService;
 
 class RequestListener
 {
     private $logger;
+    private $em;
+    private $twig;
 
-    public function __construct(LoggerService $logger)
+    private $request;
+
+    public function __construct(LoggerService $logger, EntityManagerInterface $em, TwigEnvironment $twig)
     {
         $this->logger = $logger;
+        $this->em = $em;
+        $this->twig = $twig;
     }
 
     public function onKernelRequest(RequestEvent $event)
@@ -20,11 +30,36 @@ class RequestListener
             return;
         }
 
+        $this->request = $event->getRequest();
+
         $this->logRequest();
+        if ($fullPageNoticeResponse = $this->checkPageNotices()) {
+            $event->setResponse($fullPageNoticeResponse);
+        }
     }
 
     private function logRequest()
     {
         $this->logger->log(LoggerService::REQUEST);
+    }
+
+    private function checkPageNotices()
+    {
+        $path = $this->request->getPathInfo();
+        $activeNotices = $this->em->getRepository(Notice::class)
+            ->getActiveNotices($path);
+
+        // If one of the notices is a full page notice, render the notice response and return
+        if (!preg_match('/^(\/s)?\/admin/', $path)) { // Ignore full page notices for admin urls
+            foreach ($activeNotices as $notice) {
+                if ($notice->getFullPage()) {
+                    return new Response($this->twig->render('full-page-notice.html.twig', [
+                        'message' => $notice->getMessage()
+                    ]));
+                }
+            }
+        }
+
+        $this->twig->addGlobal('global_notices', $activeNotices);
     }
 }
