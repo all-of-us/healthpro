dd73c7deb862cda26aea42f6c42d471a13498323
Shyam Naru <shyam.s.naru@vumc.org>
2020-09-02 14:15:37 -0500

    Create help videos page

--- symfony/src/Controller/HelpController.php
+++ symfony/src/Controller/HelpController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Service\HelpService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,5 +19,37 @@ class HelpController extends AbstractController
     public function index()
     {
         return $this->render('help/index.html.twig');
+    }
+
+    /**
+     * @Route("/videos", name="help_videos")
+     */
+
+    public function videosAction(SessionInterface $session)
+    {
+        $id = $session->get('sitType') === 'dv' ? 'biobank-dv' : 'biobank-hpo';
+        return $this->redirectToRoute('help_videosPlaylist', ['id' => $id]);
+    }
+
+    /**
+     * @Route("/videos/{id}", name="help_videosPlaylist")
+     */
+
+    public function videosPlaylistAction($id, Request $request, HelpService $helpService)
+    {
+        if (!array_key_exists($id, $helpService::$videoPlaylists)) {
+            throw $this->createNotFoundException('Page Not Found!');
+        }
+
+        $parameters = [
+            'videoPlaylists' => $helpService::$videoPlaylists,
+            'active' => $id
+        ];
+        if ($id === 'other') {
+            $parameters['type'] = $request->query->get('type', 'yt');
+            $parameters['helpVideosPath'] = rtrim($this->getParameter('help_videos_path'), '/');
+        }
+
+        return $this->render('help/videos.html.twig', $parameters);
     }
 }
