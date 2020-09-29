c6eb6d395d70038b3f0bd9d3e04905e318cb2062
Shyam Naru <shyam.s.naru@vumc.org>
2020-09-29 14:56:26 -0500

    Create site add/edit

--- symfony/src/Controller/SitesController.php
+++ symfony/src/Controller/SitesController.php
@@ -2,8 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
+use App\Form\SiteType;
 use App\Repository\SiteRepository;
+use App\Service\EnvironmentService;
+use App\Service\LoggerService;
+use Doctrine\ORM\EntityManagerInterface;
+use Pmi\Audit\Log;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -19,6 +28,73 @@ class SitesController extends AbstractController
         $sites = $siteRepository->findBy(['deleted' => 0], ['name' => 'asc']);
         return $this->render('admin/sites/index.html.twig', [
             'sites' => $sites
+        ]);
+    }
+
+    /**
+     * @Route("/site/{id}", name="admin_site")
+     */
+    public function edit(SiteRepository $siteRepository, EntityManagerInterface $em, LoggerService $loggerService, Request $request, ParameterBagInterface $params, EnvironmentService $env, $id = null)
+    {
+        $syncEnabled = $params->has('sites_use_rdr') ? $params->get('sites_use_rdr') : false;
+        if ($id) {
+            $site = $siteRepository->find($id);
+            if (!$site) {
+                throw $this->createNotFoundException('Page notice not found.');
+            }
+
+            if ($request->request->has('delete')) {
+                $em->remove($site);
+                $em->flush();
+                $loggerService->log(Log::SITE_DELETE, $site->getId());
+                $this->addFlash('success', 'Site removed');
+                return $this->redirectToRoute('admin_sites');
+            }
+        } else {
+            if ($syncEnabled) {
+                // can't create new sites if syncing from rdr
+                throw $this->createNotFoundException('Page notice not found.');
+            }
+            $site = null;
+        }
+        $disabled = $syncEnabled ? true : false;
+        $form = $this->createForm(SiteType::class, $site, ['disabled' => $disabled, 'isProd' => $env->isProd()]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid() && !$syncEnabled) {
+                if ($site) {
+                    $duplicateGoogleGroup = $siteRepository->getDuplicateSiteGoogleGroup($form['google_group']->getData(), $id);
+                } else {
+                    $duplicateGoogleGroup = $siteRepository->getDuplicateGoogleGroup($form['google_group']->getData());
+                }
+                if ($duplicateGoogleGroup) {
+                    $form['google_group']->addError(new FormError('This google group has already been used for another site.'));
+                }
+            }
+            if ($form->isValid()) {
+                if ($site) {
+                    $em->persist($site);
+                    $em->flush();
+                    $loggerService->log(Log::SITE_EDIT, $site->getId());
+                    $this->addFlash('success', 'Notice added');
+                } else {
+                    $site = $form->getData();
+                    $em->persist($site);
+                    $em->flush();
+                    $loggerService->log(Log::SITE_ADD, $site->getId());
+                    $this->addFlash('success', 'Notice added');
+                }
+                return $this->redirectToRoute('admin_sites');
+            } else {
+                if (count($form->getErrors()) == 0) {
+                    $form->addError(new FormError('Please correct the errors below'));
+                }
+            }
+        }
+
+        return $this->render('admin/sites/edit.html.twig', [
+            'site' => $site,
+            'siteForm' => $form->createView()
         ]);
     }
 }
---
+++ symfony/src/Form/SiteType.php
@@ -0,0 +1,141 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Site;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Validation;
+
+class SiteType extends AbstractType
+{
+    const FIXED_ANGLE = 'fixed_angle';
+    const SWINGING_BUCKET = 'swinging_bucket';
+    const FULL_DATA_ACCESS = 'full_data';
+    const LIMITED_DATA_ACCESS = 'limited_data';
+    const DOWNLOAD_DISABLED = 'disabled';
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', Type\TextType::class, [
+                'label' => 'Name',
+                'required' => true,
+                'constraints' => [
+                    new Constraints\NotBlank(),
+                    new Constraints\Type('string')
+                ],
+                'disabled' => $options['disabled'],
+            ])
+            ->add('status', Type\ChoiceType::class, [
+                'label' => 'Status',
+                'required' => true,
+                'choices' => [
+                    'Active'=> 1,
+                    'Inactive' => 0
+                ],
+                'disabled' => $options['disabled']
+            ])
+            ->add('google_group', Type\TextType::class, [
+                'label' => 'Google Group',
+                'required' => true,
+                'constraints' => [
+                    new Constraints\NotBlank(),
+                    new Constraints\Type('string')
+                ],
+                'disabled' => $options['disabled'],
+            ])
+            ->add('organization', Type\TextType::class, [
+                'label' => 'Awardee (formerly HPO ID)',
+                'required' => false,
+                'constraints' => new Constraints\Type('string'),
+                'disabled' => $options['disabled'],
+            ])
+            ->add('organization_id', Type\TextType::class, [
+                'label' => 'Organization',
+                'required' => false,
+                'constraints' => new Constraints\Type('string'),
+                'disabled' => $options['disabled'],
+            ])
+            ->add('mayolink_account', Type\TextType::class, [
+                'label' => 'MayoLINK Account',
+                'required' => false,
+                'constraints' => new Constraints\Type('string'),
+                'disabled' => $options['disabled'] && $options['isProd'],
+            ])
+            ->add('type', Type\TextType::class, [
+                'label' => 'Type (e.g. HPO, DV)',
+                'required' => false,
+                'constraints' => new Constraints\Type('string'),
+                'disabled' => $options['disabled'],
+            ])
+            ->add('email', Type\TextType::class, [
+                'label' => 'Email address(es)',
+                'required' => false,
+                'constraints' => [
+                    new Constraints\Type('string'),
+                    new Constraints\Length(['max' => 512]),
+                    new Constraints\Callback(function($list, $context) {
+                        $list = trim($list);
+                        if (empty($list)) {
+                            return;
+                        }
+                        $emails = explode(',', $list);
+                        $validator = Validation::createValidator();
+                        foreach ($emails as $email) {
+                            $email = trim($email);
+                            $errors = $validator->validate($email, new Constraints\Email());
+                            if (count($errors) > 0) {
+                                $context
+                                    ->buildViolation('Must be a comma-separated list of valid email addresses')
+                                    ->addViolation();
+                                break;
+                            }
+                        }
+                    })
+                ],
+                'disabled' => $options['disabled'] && $options['isProd'],
+            ])
+            ->add('awardee', Type\TextType::class, [
+                'label' => 'Program (e.g. STSI)',
+                'required' => false,
+                'constraints' => new Constraints\Type('string')
+            ])
+            ->add('centrifuge_type', Type\ChoiceType::class, [
+                'label' => 'Centrifuge type',
+                'required' => false,
+                'choices' => [
+                    '-- Select centrifuge type --' => null,
+                    'Fixed Angle'=> self::FIXED_ANGLE,
+                    'Swinging Bucket' => self::SWINGING_BUCKET
+                ],
+                'multiple' => false
+            ])
+            ->add('workqueue_download', Type\ChoiceType::class, [
+                'label' => 'Work Queue Download',
+                'required' => true,
+                'choices' => [
+                    'Full Data Access'=> self::FULL_DATA_ACCESS,
+                    'Limited Data Access (No PII)' => self::LIMITED_DATA_ACCESS,
+                    'Download Disabled' => self::DOWNLOAD_DISABLED
+                ],
+                'multiple' => false,
+                'constraints' => [
+                    new Constraints\NotBlank(),
+                    new Constraints\Type('string')
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Site::class,
+            'disabled' => false,
+            'isProd' => false
+        ]);
+    }
+}
