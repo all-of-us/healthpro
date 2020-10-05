7836328013ccbe81d36dfe81e7644fc058d1a058
Shyam Naru <shyam.s.naru@vumc.org>
2020-10-05 18:06:28 -0500

    Check duplicate participant id

--- symfony/src/Service/PatientStatusImportService.php
+++ symfony/src/Service/PatientStatusImportService.php
@@ -41,6 +41,9 @@ class PatientStatusImportService
             if (!preg_match("/^P\d{9}+$/", $data[0])) {
                 $form['patient_status_csv']->addError(new FormError("Invalid participant ID Format {$data[0]} in line {$row}, column 1"));
             }
+            if ($this->hasDuplicateParticipantId($patientStatuses, $data[0])) {
+                $form['patient_status_csv']->addError(new FormError("Duplicate participant ID {$data[0]} in line {$row}, column 1"));
+            }
             $patientStatus['participantId'] = $data[0];
             if (!in_array($data[1], $validStatus)) {
                 $form['patient_status_csv']->addError(new FormError("Invalid patient status {$data[1]} in line {$row}, column 2"));
@@ -67,5 +70,15 @@ class PatientStatusImportService
             array_push($rows, $row);
         }
         return $rows;
+    }
+
+    private function hasDuplicateParticipantId($patientStatuses, $participantId)
+    {
+        foreach ($patientStatuses as $patientStatus) {
+            if ($patientStatus['participantId'] === $participantId) {
+                return true;
+            }
+        }
+        return false;
     }
 }
