08aac9a3e15c6a8cd708a171e655337de0bb51e2
Shyam Naru <shyam.s.naru@vumc.org>
2020-10-06 09:01:17 -0500

    Add index for participant_id and organization columns in patient_status table

--- sql/healthpro/011-patient_status.sql
+++ sql/healthpro/011-patient_status.sql
@@ -5,5 +5,6 @@ CREATE TABLE `patient_status` (
   `awardee` varchar(50) NOT NULL,
   `history_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `history_id` (`history_id`)
+  KEY `history_id` (`history_id`),
+  KEY `participant_organization` (`participant_id`, `organization`)
 ) DEFAULT CHARSET=utf8mb4;
---
+++ sql/migrations/037-alter-patient-status-table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `patient_status` ADD INDEX participant_organization (`participant_id`, `organization`);
