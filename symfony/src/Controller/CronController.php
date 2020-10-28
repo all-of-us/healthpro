4989f83b953436c1364c2e19fa2c184b9481961d
Shyam Naru <shyam.s.naru@vumc.org>
2020-10-28 10:30:32 -0500

    Create sites and awardees/organizations sync cron jobs

--- symfony/src/Controller/CronController.php
+++ symfony/src/Controller/CronController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Service\DeceasedNotificationService;
+use App\Service\SiteSyncService;
+use Doctrine\ORM\Query\Parameter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -18,6 +21,31 @@ class CronController extends AbstractController
     {
         $deceasedNotificationService->setDeceasedStatusType($deceasedStatus);
         $deceasedNotificationService->sendEmails();
+        return $this->json(['success' => true]);
+    }
+
+    /**
+     * @Route("/sites", name="cron_sites")
+     */
+    public function sitesAction(ParameterBagInterface $params, SiteSyncService $siteSyncService)
+    {
+        if (!$params->has('sites_use_rdr')) {
+            return $this->json(['error' => 'RDR Awardee API disabled']);
+        }
+        $siteSyncService->sync();
+        return $this->json(['success' => true]);
+    }
+
+    /**
+     * @Route("/awardees-organizations", name="cron_awardees_organizations")
+     */
+    public function awardeesAndOrganizationsAction(ParameterBagInterface $params, SiteSyncService $siteSyncService)
+    {
+        if (!$params->has('sites_use_rdr')) {
+            return $this->json(['error' => 'RDR Awardee API disabled']);
+        }
+        $siteSyncService->syncAwardees();
+        $siteSyncService->syncOrganizations();
         return $this->json(['success' => true]);
     }
 }
