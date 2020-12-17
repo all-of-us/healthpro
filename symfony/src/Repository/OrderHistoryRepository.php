dee713cf1b8d52ac59391e988ea4c352fca99bf7
Shyam Naru <shyam.s.naru@vumc.org>
2020-12-17 09:27:49 -0600

    Remove unused code in OrderHistoryRepository

--- symfony/src/Repository/OrderHistoryRepository.php
+++ symfony/src/Repository/OrderHistoryRepository.php
@@ -18,33 +18,4 @@ class OrderHistoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, OrderHistory::class);
     }
-
-    // /**
-    //  * @return OrderHistory[] Returns an array of OrderHistory objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('o.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?OrderHistory
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
