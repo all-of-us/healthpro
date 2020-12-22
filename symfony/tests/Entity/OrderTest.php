fa4842bd0a4b7565faa22d0468258665518d8745
Shyam Naru <shyam.s.naru@vumc.org>
2020-12-22 10:23:49 -0600

    Assert RDR object for HPO order

--- symfony/tests/Entity/OrderTest.php
+++ symfony/tests/Entity/OrderTest.php
@@ -48,23 +48,37 @@ class OrderTest extends TestCase
 
     public function testRdrObject()
     {
-        $order = $this->createOrder([
+        $orderArray = [
             'user' => $this->getUser(),
             'site' => 'test',
             'ts' => new \DateTime('2020-12-21 08:00:00'),
             'participantId' => 'P123456789',
-            'rdrId' => 'WEB12345',
+            'rdrId' => 'WEB123456789',
             'biobankId' => 'Y123456789',
             'orderId' => '0123456789',
-            'mayoId' => 'WEB12345',
+            'mayoId' => 'WEB123456789',
             'collectedSamples' => '["1SS08","1PS08","1HEP4","1ED04","1ED10","1CFD9","1PXR2","1UR10"]',
             'processedSamples' => '["1SS08","1PS08"]',
             'processedSamplesTs' => '{"1SS08":1606753560,"1PS08":1606753560}',
             'processedCentrifugeType' => 'swinging_bucket',
             'finalizedSamples' => '["1SS08","1PS08","1HEP4","1ED04","1ED10","1CFD9","1PXR2","1UR10"]',
             'version' => '3.1'
-        ]);
+        ];
+        // HPO order
+        $order = $this->createOrder($orderArray);
         $order->loadSamplesSchema();
         $this->assertEquals('Patient/P123456789', $order->getRdrObject()->subject);
+        // Assert createdInfo
+        $this->assertEquals('test@example.com', $order->getRdrObject()->createdInfo['author']['value']);
+        $this->assertEquals('hpo-site-test', $order->getRdrObject()->createdInfo['site']['value']);
+        // Assert identifiers orderId and mayoId
+        $this->assertEquals('0123456789', $order->getRdrObject()->identifier[0]['value']);
+        $this->assertEquals('WEB123456789', $order->getRdrObject()->identifier[1]['value']);
+        // Assert processed sample codes
+        $this->assertEquals('1SST8', $order->getRdrObject()->samples[0]['test']);
+        $this->assertEquals('1PST8', $order->getRdrObject()->samples[1]['test']);
+        // Assert processingRequired
+        $this->assertEquals('1', $order->getRdrObject()->samples[0]['processingRequired']);
+        $this->assertEquals('1', $order->getRdrObject()->samples[1]['processingRequired']);
     }
 }
