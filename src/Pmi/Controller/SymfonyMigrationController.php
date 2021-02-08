308f7d7625700a31a2e83651928718e796e9bb7a
Shyam Naru <shyam.s.naru@vumc.org>
2021-02-08 13:30:11 -0600

    Remove WQ and WQ export silex routes and views

--- src/Pmi/Controller/SymfonyMigrationController.php
+++ src/Pmi/Controller/SymfonyMigrationController.php
@@ -18,8 +18,8 @@ class SymfonyMigrationController extends AbstractController
         ]],
         ['admin_home', '/admin'],
         ['orderCheck', '/participant/{participantId}/order/check'],
-        ['order', '/participant/{participantId}/order/{orderId}']
-
+        ['order', '/participant/{participantId}/order/{orderId}'],
+        ['workqueue_index', '/workqueue', ['method' => 'GET|POST']],
     ];
 
     public function deceased_reports_indexAction(Application $app)
@@ -105,5 +105,14 @@ class SymfonyMigrationController extends AbstractController
             $participantId,
             $orderId
         ));
+    }
+
+
+    /**
+     * @deprecated 2021-02-08
+     */
+    public function workqueue_indexAction(Application $app)
+    {
+        return $app->redirect('/s/workqueue');
     }
 }
