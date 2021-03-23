3d5142c7109235908c40df62e19ebeb7c057996f
Shyam Naru <shyam.s.naru@vumc.org>
2021-03-23 15:41:22 -0500

    Fix unit tests

--- src/Pmi/Evaluation/Fhir.php
+++ src/Pmi/Evaluation/Fhir.php
@@ -1157,7 +1157,7 @@ class Fhir
 
     protected function getEffectiveDateTime($field, $replicate = 1)
     {
-        if ($this->data->{$field} === 'ehr' && !empty($this->data->{$field . '-ehr-date'}) && $replicate == 1) {
+        if (isset($this->data->{$field}) && $this->data->{$field} === 'ehr' && !empty($this->data->{$field . '-ehr-date'}) && $replicate == 1) {
             return $this->data->{$field . '-ehr-date'}->format('Y-m-d\TH:i:s\Z');
         }
         return $this->date;
