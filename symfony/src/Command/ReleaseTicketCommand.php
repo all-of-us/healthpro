e37afeb93168c8c8e8c42a321420ba34a24a09b7
Jason Tan <jason.tan@vumc.org>
2021-04-26 17:11:43 -0500

    Add command for creating JIRA release ticket

---
+++ symfony/src/Command/ReleaseTicketCommand.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\JiraService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Twig\Environment as Templating;
+
+class ReleaseTicketCommand extends Command
+{
+    private $jira;
+    private $io;
+    private $templating;
+
+    public function __construct(JiraService $jira, Templating $templating)
+    {
+        $this->jira = $jira;
+        $this->templating = $templating;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('pmi:jira')
+            ->setDescription('Create JIRA release ticket');
+    }
+
+    private function selectVersion(array $versions): ?string
+    {
+        $defaultVersion = null;
+        if (isset($versions[0]) && !$versions[0]->released) {
+            $defaultVersion = $versions[0]->name;
+        }
+        $tableHeaders = ['Version', 'Released?', 'Release Date', 'URL'];
+        $tableRows = [];
+        foreach ($versions as $version) {
+            $tableRows[] = [
+                $version->name,
+                $version->released ? '✓' : '',
+                $version->releaseDate ?? '',
+                sprintf('https://precisionmedicineinitiative.atlassian.net/projects/HPRO/versions/%s', $version->id)
+            ];
+        }
+        $this->io->table($tableHeaders, $tableRows);
+
+        return $this->io->ask('Which version are you releasing?', $defaultVersion);
+    }
+
+    private function getIssues(string $version): array
+    {
+        $jiraIssues = $this->jira->getIssuesByVersion($version);
+        $tableHeaders = ['ID', 'Title', 'Type', 'Status'];
+        $tableRows = [];
+        $issues = [];
+        foreach ($jiraIssues as $jiraIssue) {
+            $issue = (object)[
+                'id' => $jiraIssue->key,
+                'title' => $jiraIssue->fields->summary,
+                'type' => $jiraIssue->fields->issuetype->name,
+                'status' => $jiraIssue->fields->status->name,
+            ];
+            $tableRows[] = [$issue->id, $issue->title, $issue->type, $issue->status];
+            $issues[] = $issue;
+        }
+        $this->io->table($tableHeaders, $tableRows);
+
+        return $issues;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $this->io->section('Unreleased and recent versions');
+        $versions = $this->jira->getVersions(5);
+        if (empty($versions)) {
+            $this->io->warning('Could not retrieve versions');
+        }
+
+        $version = $this->selectVersion($versions);
+        if ($version === null) {
+            $this->io->warning('No version selected.');
+            return 1;
+        }
+
+        $this->io->section(sprintf('Tickets for release %s', $version));
+
+        $issues = $this->getIssues($version);
+
+        if (!$this->io->confirm('Does this look right?')) {
+            $this->io->comment('Exiting.');
+            return 1;
+        }
+
+        $description = $this->templating->render('jira/release.txt.twig', [
+            'issues' => $issues,
+            'releaseDate' => new \DateTime('+2 days'), // TODO: from user input or version release date
+            'completeDate' => new \DateTime()
+        ]);
+        $this->io->block($description);
+        $this->io->success('Done');
+
+        return 0;
+    }
+}
---
+++ symfony/src/Service/JiraService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class JiraService
+{
+    private $client;
+
+    public function __construct(ParameterBagInterface $params, LoggerInterface $logger)
+    {
+        if (!$params->has('jira_api_user') || !$params->has('jira_api_token')) {
+            $logger->warning('Missing JIRA API configuration. See config.yml.dist for details.');
+            return;
+        }
+        $this->client = new Client([
+            'base_uri' => 'https://precisionmedicineinitiative.atlassian.net/rest/api/latest/',
+            'auth' => [$params->get('jira_api_user'), $params->get('jira_api_token')]
+        ]);
+    }
+
+    public function getVersions(int $count = 10): array
+    {
+        $response = $this->client->request('GET', 'project/HPRO/version', [
+            'query' => [
+                'orderBy' => '-releaseDate',
+                'maxResults' => $count
+            ]
+        ]);
+        $responseObject = json_decode($response->getBody()->getContents());
+
+        return $responseObject->values ?? [];
+    }
+
+    public function getIssuesByVersion(string $version): array
+    {
+        $jql = sprintf('project=%s AND fixVersion=%s', 'HPRO', $version);
+        $response = $this->client->request('GET', 'search', [
+            'query' => [
+                'jql' => $jql,
+                'fields' => 'issuetype,status,summary'
+            ]
+        ]);
+        $responseObject = json_decode($response->getBody()->getContents());
+
+        return $responseObject->issues ?? [];
+    }
+}
