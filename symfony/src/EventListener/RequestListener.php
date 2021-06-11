6e545395c3cc65e0f9892c0b5bd4460650949ea9
Shyam Naru <shyam.s.naru@vumc.org>
2021-06-11 13:49:30 -0500

    Migrate site select check logic

--- symfony/src/EventListener/RequestListener.php
+++ symfony/src/EventListener/RequestListener.php
@@ -2,11 +2,14 @@
 
 namespace App\EventListener;
 
+use App\Service\SiteService;
+use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Twig\Environment as TwigEnvironment;
 use App\Entity\Notice;
 use App\Service\LoggerService;
@@ -17,15 +20,21 @@ class RequestListener
     private $em;
     private $twig;
     private $session;
+    private $userService;
+    private $siteService;
+    private $authorizationChecker;
 
     private $request;
 
-    public function __construct(LoggerService $logger, EntityManagerInterface $em, TwigEnvironment $twig, SessionInterface $session)
+    public function __construct(LoggerService $logger, EntityManagerInterface $em, TwigEnvironment $twig, SessionInterface $session, UserService $userService, SiteService $siteService, AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->logger = $logger;
         $this->em = $em;
         $this->twig = $twig;
         $this->session = $session;
+        $this->userService = $userService;
+        $this->siteService = $siteService;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public function onKernelRequest(RequestEvent $event)
@@ -74,9 +83,33 @@ class RequestListener
 
     private function checkSiteSelect()
     {
-        if (!$this->session->has('site') && !$this->session->has('awardee') && !preg_match('/^(\/s)?\/(_profiler|_wdt|cron|admin|help|settings|problem|biobank|review|workqueue|site|login)($|\/).*/',
-                $this->request->getPathInfo())) {
-            return new RedirectResponse('/');
+        if (!$this->session->has('site') && !$this->session->has('awardee') && ($this->authorizationChecker->isGranted('ROLE_USER') || $this->authorizationChecker->isGranted('ROLE_AWARDEE'))) {
+            $user = $this->userService->getUser();
+            if (count($user->getSites()) === 1 && empty($user->getAwardees()) && $this->siteService->isValidSite($user->getSites()[0]->email)) {
+                $this->siteServiceswitchSite($user->getSites()[0]->email);
+            } elseif (count($user->getAwardees()) === 1 && empty($user->getSites())) {
+                $this->switchSite($user->getAwardees()[0]->email);
+            } elseif (!preg_match('/^(\/s)?\/(_profiler|_wdt|cron|admin|help|settings|problem|biobank|review|workqueue|site|login|site_select)($|\/).*/',
+                    $this->request->getPathInfo()) && !$this->isUpkeepRoute()) {
+                return new RedirectResponse('/s/site/select');
+            }
         }
+    }
+
+    /**
+     * "Upkeep" routes are routes that we typically want to allow through
+     * even when workflow dictates otherwise.
+     */
+    public function isUpkeepRoute()
+    {
+        $route = $this->request->attributes->get('_route');
+        return (in_array($route, [
+                'logout',
+                'loginReturn',
+                'timeout',
+                'keepAlive',
+                'clientTimeout',
+                'agreeUsage'
+            ]));
     }
 }
