cfa181a95b6c1e090a9f10106111c85a00beb02d
Shyam Naru <shyam.s.naru@vumc.org>
2021-06-22 11:03:02 -0500

    Migrate dash splash test cases

--- symfony/tests/Application/HealthProApplicationTest.php
+++ symfony/tests/Application/HealthProApplicationTest.php
@@ -67,4 +67,36 @@ class HealthProApplicationTest extends WebTestCase
         self::assertEquals(1, count($crawler->filter('#dashSplashSelector')));
     }
 
+    public function testDashSplashAwardee()
+    {
+        $this->logIn('testDashSplashAwardee@example.com', [
+            new GoogleGroup('awardee-1@gapps.com', 'Test Group 1', 'lorem ipsum 1'),
+            new GoogleGroup(User::DASHBOARD_GROUP . '@gapps.com', 'Test Group 2', 'lorem ipsum 2')
+        ]);
+        $this->client->followRedirects();
+        $crawler = $this->client->request('GET', '/s');
+        self::assertEquals(1, count($crawler->filter('#dashSplashSelector')));
+    }
+
+    public function testDashSplashAdmin()
+    {
+        $this->logIn('testDashSplashAdmin@example.com', [
+            new GoogleGroup(User::ADMIN_GROUP . '@gapps.com', 'Test Group 1', 'lorem ipsum 1'),
+            new GoogleGroup(User::DASHBOARD_GROUP . '@gapps.com', 'Test Group 2', 'lorem ipsum 2')
+        ]);
+        $this->client->followRedirects();
+        $crawler = $this->client->request('GET', '/s');
+        self::assertEquals(1, count($crawler->filter('#dashSplashSelector')));
+    }
+
+    public function testDashSplashDvAdmin()
+    {
+        $this->logIn('testDashSplashDvAdmin@example.com', [
+            new GoogleGroup(User::ADMIN_DV . '@gapps.com', 'Test Group 1', 'lorem ipsum 1'),
+            new GoogleGroup(User::DASHBOARD_GROUP . '@gapps.com', 'Test Group 2', 'lorem ipsum 2')
+        ]);
+        $this->client->followRedirects();
+        $crawler = $this->client->request('GET', '/s');
+        self::assertEquals(1, count($crawler->filter('#dashSplashSelector')));
+    }
 }
