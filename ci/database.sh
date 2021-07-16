fccfcf29a1797fddb2c2ef48ead9e6dcee2742d0
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-07-16 15:55:16 -0500

    Fix typo

--- ci/database.sh
+++ ci/database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
-bin/console doctrine:database:create --if-not-exist -n
+bin/console doctrine:database:create --if-not-exists -n
 bin/console doctrine:migrations:migrate -n
