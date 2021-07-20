1ad85a69bd2e5ba956bda4ddda7237a1d25ca781
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-07-20 11:31:37 -0500

    Ensure migration runs as test

--- ci/database.sh
+++ ci/database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
-bin/console doctrine:database:create --if-not-exists -n
-bin/console doctrine:migrations:migrate -n
+APP_ENV=test bin/console doctrine:database:create --if-not-exists -n
+APP_ENV=test bin/console doctrine:migrations:migrate -n
