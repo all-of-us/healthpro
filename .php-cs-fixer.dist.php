a5161a2f54e0c02ea8fd6cd8c3a7c978797aeeee
Shyam Naru <shyam.s.naru@vumc.org>
2021-08-16 15:47:53 -0500

    Set rule to PSR-12 standard

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,12 @@
 <?php
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
+    ->in(__DIR__ . '/symfony/src')
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@Symfony' => true,
+        '@PSR12' => true,
     ])
     ->setFinder($finder)
 ;
