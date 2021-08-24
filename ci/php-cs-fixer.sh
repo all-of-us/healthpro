d94e3d7bbd8fc98124f206e5d454d77fe432693c
Shyam Naru <shyam.s.naru@vumc.org>
2021-08-24 16:52:49 -0500

    Change file name in bash command

--- ci/php-cs-fixer.sh
+++ ci/php-cs-fixer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./tools/php-cs-fixer/vendor/bin/php-cs-fixer fix --dry-run --format=junit > /tmp/circleci-test-results/phpunit.xml
+./tools/php-cs-fixer/vendor/bin/php-cs-fixer fix --dry-run --format=junit > /tmp/circleci-test-results/php-cs-fixer.xml
