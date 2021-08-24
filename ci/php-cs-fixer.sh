4e355c57fbb134a9653b81885e22966a2e176fda
Shyam Naru <shyam.s.naru@vumc.org>
2021-08-24 13:53:16 -0500

    Fix php-cs-fixer bash command

--- ci/php-cs-fixer.sh
+++ ci/php-cs-fixer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./tools/php-cs-fixer/vendor/bin/php-cs-fixer fix --dry-run -v --log-junit /tmp/circleci-test-results/phpunit.xml
+./tools/php-cs-fixer/vendor/bin/php-cs-fixer fix --dry-run --format=junit > /tmp/circleci-test-results/phpunit.xml
