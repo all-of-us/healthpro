ed3a96627adc1370fd4c98259eb48b747b15de8b
Shyam Naru <shyam.s.naru@vumc.org>
2021-10-21 16:39:07 -0500

    Change id format

--- web/assets/js/views/workqueue.js
+++ web/assets/js/views/workqueue.js
@@ -148,13 +148,13 @@ $(document).ready(function() {
         pageLength: 25,
         drawCallback: function () {
             var pageInfo = table.page.info();
-            $('#total-pages').text(pageInfo.pages);
+            $('#total_pages').text(pageInfo.pages);
             var dropDownHtml = '';
             for (var count = 1; count <= pageInfo.pages; count++) {
                 var pageNumber = count - 1;
                 dropDownHtml += '<option value="' + pageNumber + '">' + count + '</option>';
             }
-            var pageDropDown = $('#page-drop-down');
+            var pageDropDown = $('#page_drop_down');
             pageDropDown.html(dropDownHtml);
             pageDropDown.val(pageInfo.page);
         },
@@ -319,7 +319,7 @@ $(document).ready(function() {
         ]
     });
 
-    $('#page-drop-down').change(function () {
+    $('#page_drop_down').change(function () {
         table.page(parseInt($(this).val())).draw('page');
     });
 
