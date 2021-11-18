08c634aadc512ec7ad9b657d29da8a30304ea0b1
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-11-17 18:32:49 -0600

    Wire in PHPStan step.

---
+++ ci/phpstan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+
+cp $APP_DIR/phpstan.neon.dist $APP_DIR/phpstan.neon
+
+sed -i 's/var\/cache\/dev/var\/cache\/test/gI' $APP_DIR/phpstan.neon
+sed -i 's/App_KernelDevDebugContainer/App_KernelTestDebugContainer/gI' $APP_DIR/phpstan.neon
+./vendor/bin/phpstan analyse --memory-limit 2G
