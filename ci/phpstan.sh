8bceb9132ae5ae73f18e21b9b23d991cafb91611
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-11-17 22:48:34 -0600

    Adjust APP_ENV

--- ci/phpstan.sh
+++ ci/phpstan.sh
@@ -5,4 +5,4 @@ cp $APP_DIR/phpstan.neon.dist $APP_DIR/phpstan.neon
 
 sed -i 's/var\/cache\/dev/var\/cache\/test/gI' $APP_DIR/phpstan.neon
 sed -i 's/App_KernelDevDebugContainer/App_KernelTestDebugContainer/gI' $APP_DIR/phpstan.neon
-./vendor/bin/phpstan analyse --memory-limit 2G
+APP_ENV=test ./vendor/bin/phpstan analyse --memory-limit 2G
