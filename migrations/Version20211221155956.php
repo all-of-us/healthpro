1cf6c3af581bba9da351b2681bb044f5155af8a2
Shyam Naru <shyam.s.naru@vumc.org>
2021-12-21 10:28:36 -0600

    Create migration to add state column in sites table

---
+++ migrations/Version20211221155956.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20211221155956 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add state column in Site entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE sites ADD COLUMN `state` varchar(255) NULL DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE sites DROP COLUMN `state`');
+    }
+}
