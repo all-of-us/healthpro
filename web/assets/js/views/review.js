87a1cb57b72f2d8de6db3f2f60b12a2d8b40f021
Shyam Naru <shyam.s.naru@vumc.org>
2022-01-04 16:34:37 -0600

    Use ajax to load other names in review tabs

---
+++ web/assets/js/views/review.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    const reviewTable = $('.table-review');
+    const nameLookupUrl = reviewTable.data('name-lookup-url');
+    const missingName = reviewTable.data('missing-name');
+
+    $('.load-name').each(function () {
+        const td = $(this);
+        $.getJSON(nameLookupUrl + td.data('participant-id'), function (data) {
+            td.empty();
+            if (data && data.lastName && data.firstName) {
+                const a = $('<a>')
+                    .attr('href', td.data('href'))
+                    .text(data.lastName + ', ' + data.firstName);
+                td.append(a);
+            } else {
+                td.text(missingName);
+            }
+        })
+            .fail(function () {
+                td.html('<em>Error loading name</em>');
+            });
+    });
+});
