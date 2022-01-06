7a44ee226c90cbb1088fb218efb82adb2e864333
Shyam Naru <shyam.s.naru@vumc.org>
2022-01-06 16:28:50 -0600

    Remove unused code from Log & LoggerService class

--- src/Service/LoggerService.php
+++ src/Service/LoggerService.php
@@ -7,6 +7,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Audit\Log;
 
 class LoggerService
 {
@@ -17,47 +18,6 @@ class LoggerService
     protected $env;
     protected $action;
     protected $data;
-
-    public const PMI_AUDIT_PREFIX = 'PMI_AUDIT_';
-
-    // Actions
-    public const REQUEST = 'REQUEST';
-    public const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-    public const LOGIN_FAIL = 'LOGIN_FAIL';
-    public const LOGOUT = 'LOGOUT';
-    public const INVALID_IP = 'INVALID_IP';
-    public const ORDER_CREATE = 'ORDER_CREATE';
-    public const ORDER_EDIT = 'ORDER_EDIT';
-    public const ORDER_HISTORY_CREATE = 'ORDER_HISTORY_CREATE';
-    public const EVALUATION_CREATE = 'EVALUATION_CREATE';
-    public const EVALUATION_EDIT = 'EVALUATION_EDIT';
-    public const EVALUATION_DELETE = 'EVALUATION_DELETE';
-    public const EVALUATION_HISTORY_CREATE = 'EVALUATION_HISTORY_CREATE';
-    public const SITE_EDIT = 'SITE_EDIT';
-    public const SITE_ADD = 'SITE_ADD';
-    public const SITE_DELETE = 'SITE_DELETE';
-    public const WORKQUEUE_EXPORT = 'WORKQUEUE_EXPORT';
-    public const CROSS_ORG_PARTICIPANT_ATTEMPT = 'CROSS_ORG_PARTICIPANT_ATTEMPT';
-    public const CROSS_ORG_PARTICIPANT_AGREE = 'CROSS_ORG_PARTICIPANT_AGREE';
-    public const CROSS_ORG_PARTICIPANT_VIEW = 'CROSS_ORG_PARTICIPANT_VIEW';
-    public const WITHDRAWAL_NOTIFY = 'WITHDRAWAL_NOTIFY';
-    public const DEACTIVATE_NOTIFY = 'DEACTIVATE_NOTIFY';
-    public const PROBLEM_CREATE = 'PROBLEM_CREATE';
-    public const PROBLEM_EDIT = 'PROBLEM_EDIT';
-    public const PROBLEM_COMMENT_CREATE = 'PROBLEM_COMMENT_CREATE';
-    public const PROBLEM_NOTIFIY = 'PROBLEM_NOTIFIY';
-    public const QUEUE_RESEND_EVALUATION = 'QUEUE_RESEND_EVALUATION';
-    public const MISSING_MEASUREMENTS_ORDERS_NOTIFY = 'MISSING_MEASUREMENTS_ORDERS_NOTIFY';
-    public const NOTICE_EDIT = 'NOTICE_EDIT';
-    public const NOTICE_ADD = 'NOTICE_ADD';
-    public const NOTICE_DELETE = 'NOTICE_DELETE';
-    public const PATIENT_STATUS_ADD = 'PATIENT_STATUS_ADD';
-    public const PATIENT_STATUS_EDIT = 'PATIENT_STATUS_EDIT';
-    public const PATIENT_STATUS_HISTORY_ADD = 'PATIENT_STATUS_HISTORY_ADD';
-    public const PATIENT_STATUS_HISTORY_EDIT = 'PATIENT_STATUS_HISTORY_EDIT';
-    public const AWARDEE_ADD = 'AWARDEE_ADD';
-    public const ORGANIZATION_ADD = 'ORGANIZATION_ADD';
-    public const CSRF_TOKEN_MISMATCH = 'CSRF_TOKEN_MISMATCH';
 
     public function __construct(
         LoggerInterface $logger,
@@ -78,7 +38,7 @@ class LoggerService
         $this->action = $action;
         $this->data = $data;
         $this->logSyslog();
-        if (!$this->env->values['isUnitTest'] && !$this->env->isPhpDevServer() && $action != self::REQUEST) {
+        if (!$this->env->values['isUnitTest'] && !$this->env->isPhpDevServer() && $action != Log::REQUEST) {
             $this->logDatastore();
         }
     }
@@ -100,7 +60,7 @@ class LoggerService
         $syslogData[] = $logArray['ip'];
         $syslogData[] = $logArray['user'];
         $syslogData[] = $logArray['site'];
-        $syslogData[] = '[' . self::PMI_AUDIT_PREFIX . $logArray['action'] . ']';
+        $syslogData[] = '[' . Log::PMI_AUDIT_PREFIX . $logArray['action'] . ']';
         if ($logArray['data']) {
             $syslogData[] = json_encode($logArray['data']);
         }
