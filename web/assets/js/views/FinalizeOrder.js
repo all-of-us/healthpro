ffd6cda6e3ae148cda7d597ddf41f734d382448f
Shyam Naru <shyam.s.naru@vumc.org>
2022-01-14 14:20:39 -0600

    Move finalize twig js code into separate file

---
+++ web/assets/js/views/FinalizeOrder.js
@@ -0,0 +1,129 @@
+$(document).ready(function () {
+    var order = $('#order_finalize');
+    var orderType = order.data('order-type');
+    var orderCollectTime = order.data('order-collect-time');
+    var orderSite = order.data('order-site');
+    var userSite = order.data('user-site');
+    var name = 'input[name="order[finalizedSamples][]"]';
+    var sampleWarning = 'At least one sample that was collected and processed (as applicable) was not finalized. ';
+    if (orderType === 'saliva') {
+        sampleWarning = 'At least one sample that was collected (as applicable) was not finalized. ';
+        $('input.sample-disabled').closest('label').addClass('text-warning');
+        var html;
+        var collected = $(name).attr('collected');
+        if (typeof collected !== 'undefined') {
+            html = '<label><b>Collected:</b></label> <i class="fa fa-check text-success" aria-hidden="true"></i> ' + collected + '</span>';
+        } else {
+            html = '<label><b>Collected:</b></label> <span class="label-normal text-danger">Not collected</span></span>';
+        }
+        $(name).closest('div.checkbox').append('<span class="saliva-info">' + html + '</span>');
+    } else {
+        $('.sample-disabled label').addClass('text-warning');
+        $('.header-collected').append('<br><small>' + orderCollectTime + '</small>');
+        $('.samples').each(function (e) {
+            var html;
+            var collected = $(this).find(name).attr('collected');
+            var processed = $(this).find(name).attr('processed');
+            var requiredProcessing = $(this).find(name).attr('required-processing');
+            var warning = $(this).find(name).attr('warning');
+            var error = $(this).find(name).attr('error');
+            if (typeof collected !== 'undefined') {
+                html = '<td align="center"><i class="fa fa-check text-success" aria-hidden="true"></i></td>';
+            } else {
+                html = '<td align="center"><label class="label-normal text-warning">Not collected</label></td>';
+            }
+            if (typeof processed !== 'undefined') {
+                if (typeof error !== 'undefined') {
+                    html += '<td><span class="text-danger"><i class="fa fa-exclamation-circle" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="' + error + '"></i> ' + processed + '</span></td>';
+                } else if (typeof warning !== 'undefined') {
+                    html += '<td><span class="text-warning"><i class="fa fa-exclamation-triangle" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="' + warning + '"></i> ' + processed + '</span></td>';
+                } else {
+                    html += '<td><i class="fa fa-check text-success" aria-hidden="true"></i> ' + processed + '</td>';
+                }
+            } else {
+                if (typeof requiredProcessing !== 'undefined') {
+                    html += '<td><label class="label-normal text-warning">Not processed</label></td>';
+                }
+            }
+            $(this).find('td:last').after(html);
+        });
+    }
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#checkall').on('change', function () {
+        $('#order_finalizedSamples input:checkbox:enabled').prop('checked', $(this).prop('checked'));
+        showHideConfirmEmptyOrderCheck();
+    });
+    $('#order_finalizedTs').pmiDateTimePicker();
+
+    new PMI.views['OrderSubPage']({
+        el: $("body")
+    });
+
+    $('#enable-number').on('click', function () {
+        $('#enable-number').parent().addClass('active');
+        $('#enable-barcode').parent().removeClass('active');
+        $('#fedex-barcode').hide();
+        $('#fedex-number').removeClass('col-xs-6').addClass('col-xs-12');
+        $('#fedex-number input').attr('readonly', false);
+        return false;
+    });
+    $('#enable-barcode').on('click', function () {
+        $('#enable-barcode').parent().addClass('active');
+        $('#enable-number').parent().removeClass('active');
+        $('#fedex-barcode').show();
+        $('#fedex-number').removeClass('col-xs-12').addClass('col-xs-6');
+        $('#fedex-number input').attr('readonly', true);
+        return false;
+    });
+
+    $('#fedex_barcode_first, #fedex_barcode_second').on('change keyup', function () {
+        var target;
+        if ($(this).attr('id') === 'fedex_barcode_second') {
+            target = $('#order_fedexTracking_second');
+        } else {
+            target = $('#order_fedexTracking_first');
+        }
+        target.attr('placeholder', '');
+        target.parent().removeClass('has-error').removeClass('has-success');
+        var barcode = $(this).val().trim();
+        if (barcode.match(/^[a-zA-Z0-9]{18}$/)) { // 18 digit alphanumeric barcode for UPS
+            target.val(barcode);
+            target.parent().addClass('has-success');
+        }
+        else if (barcode.match(/^[0-9]{34}$/)) { // 34 digit barcode for FedEx
+            var fedexTracking = barcode.substring(20);
+            fedexTracking = fedexTracking.replace(/^0{1,2}/, ''); // trim up to two leading 0's
+            target.val(fedexTracking);
+            target.parent().addClass('has-success');
+        } else {
+            target.val('');
+            if (barcode) {
+                target.attr('placeholder', 'Invalid barcode');
+                target.parent().addClass('has-error');
+            }
+        }
+    });
+
+    $('.finalize-form button[type="submit"]').on('click', function () {
+        //Display warning message
+        var confirmMessage = 'Are you sure you want to finalize this order?';
+        var message = confirmMessage;
+        if (userSite !== orderSite) {
+            var siteWarning = 'The order creation site and finalization site do not match. ';
+            message = 'Warning! ' + siteWarning + confirmMessage;
+        }
+        $('input[name="order[finalizedSamples][]"]').each(function () {
+            //Select samples that are unchecked and not disabled
+            if ($(this).prop('checked') === false && $(this).prop('disabled') === false) {
+                message = 'Warning! ' + sampleWarning + confirmMessage;
+                if (typeof siteWarning !== 'undefined' && siteWarning) {
+                    message = 'Warning! 1) ' + siteWarning + '2) ' + sampleWarning + confirmMessage;
+                }
+                return false;
+            }
+        });
+        return confirm(message);
+    });
+});
