7983781d7e9569c166fdbb183d9a045b013d3f92
Shyam Naru <shyam.s.naru@vumc.org>
2022-03-17 08:58:26 -0500

    Optimize route check

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -8,6 +8,7 @@ class BaseController extends AbstractController
 {
     protected function isReadOnly(): bool
     {
-        return strpos($this->container->get('request_stack')->getCurrentRequest()->get('_route'), 'read_') === 0 ? true : false;
+        $route = $this->container->get('request_stack')->getCurrentRequest()->get('_route');
+        return strpos($route, 'read_') === 0;
     }
 }
