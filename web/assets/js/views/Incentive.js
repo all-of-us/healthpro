f8fac654ae409c2e23fe1d1cf18e8122e76bc72e
Shyam Naru <shyam.s.naru@vumc.org>
2022-03-18 14:55:50 -0500

    Display remove/amend warning pop up

--- web/assets/js/views/Incentive.js
+++ web/assets/js/views/Incentive.js
@@ -6,4 +6,13 @@ $(document).ready(function () {
     $('#incentive .panel-collapse').on('hide.bs.collapse', function () {
         $(this).siblings('.panel-heading').removeClass('active');
     });
+
+    $(".incentive-modify").on('click', function (e) {
+        e.preventDefault();
+        var url = $(e.currentTarget).data('href');
+        var type = $(e.currentTarget).data('type');
+        $('#incentiveModifyModel .modal-body').html('Are you sure you want to ' + type + ' this incentive occurance?');
+        $('#incentiveModifyModel .incentive-ok').attr('href', url);
+        $('#incentiveModifyModel').modal('show');
+    });
 });
