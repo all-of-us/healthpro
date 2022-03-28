2c58311a636bc7495dd0b5527343c24dff7b7849
Shyam Naru <shyam.s.naru@vumc.org>
2022-03-28 11:18:33 -0500

    Remove repository class

--- migrations/Version20220304191506.php
+++ migrations/Version20220304191506.php
@@ -7,14 +7,11 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220304191506 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create Incentive entity';
     }
 
     public function up(Schema $schema): void
--- src/Entity/Incentive.php
+++ src/Entity/Incentive.php
@@ -6,7 +6,7 @@ use App\Repository\IncentiveRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=IncentiveRepository::class)
+ * @ORM\Entity
  */
 class Incentive
 {
