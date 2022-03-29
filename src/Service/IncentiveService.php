205226ae3aafc175ee84e3bfeae3ccbe8c4b918f
Shyam Naru <shyam.s.naru@vumc.org>
2022-03-29 16:55:35 -0500

    Create incentive service class and methods for create/amend/cancel incentives

---
+++ src/Service/IncentiveService.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Audit\Log;
+
+class IncentiveService
+{
+    protected $rdrApiService;
+    protected $siteService;
+    protected $userService;
+    protected $em;
+    protected $loggerService;
+
+    public function __construct(
+        RdrApiService $rdrApiService,
+        SiteService $siteService,
+        UserService $userService,
+        EntityManagerInterface $em,
+        LoggerService $loggerService
+    ) {
+        $this->rdrApiService = $rdrApiService;
+        $this->siteService = $siteService;
+        $this->userService = $userService;
+        $this->em = $em;
+        $this->loggerService = $loggerService;
+    }
+
+    public function getIncentivesHistory($participantId, $organizationId)
+    {
+        try {
+            $response = $this->rdrApiService->get("rdr/v1/PatientStatus/{$participantId}/Organization/{$organizationId}/History");
+            $result = json_decode($response->getBody()->getContents());
+            if (is_array($result)) {
+                return $result;
+            }
+        } catch (\Exception $e) {
+            $this->rdrApiService->logException($e);
+            return false;
+        }
+        return false;
+    }
+
+    public function getRdrObject($incentive, $type = 'create')
+    {
+        $obj = new \StdClass();
+        $email = $this->userService->getUser()->getEmail();
+        $now = new \DateTime();
+        if ($type !== 'create') {
+            $obj->incentiveId = $incentive->getRdrRid();
+        }
+        if ($type === 'cancel') {
+            $obj->cancelledBy = $email;
+            $obj->cancelledDate = $now;
+            $obj->cancel = true;
+        } else {
+            $obj->createdBy = $email;
+            $obj->site = $this->siteService->getSiteIdWithPrefix();
+            $obj->dateGiven = $incentive->getIncentiveDateGiven();
+            $obj->occurrence = $incentive->getOtherIncentiveOccurrence() ?? $incentive->getIncentiveOccurrence();
+            $obj->incentiveType = $incentive->getOtherIncentiveType() ?: $incentive->getIncentiveType();
+            if ($incentive->getGiftCardType()) {
+                $obj->giftCardType = $incentive->getGiftCardType();
+            }
+            $obj->amount = $incentive->getIncentiveAmount();
+            $obj->notes = $incentive->notes;
+        }
+        return $obj;
+    }
+
+    public function createIncentive($participantId, $incentiveForm)
+    {
+        $incentive = $incentiveForm->getData();
+        if ($incentive->getIncentiveAmount() === 'other') {
+            $incentive->setIncentiveAmount($incentiveForm['other_incentive_amount']->getData());
+        }
+        $postData = $this->getRdrObject($incentive);
+        try {
+            $response = $this->rdrApiService->post("rdr/v1/Participant/{$participantId}/Incentives", $postData);
+            $result = json_decode($response->getBody()->getContents());
+            if (is_object($result) && isset($result->authored)) {
+                $now = new \DateTime();
+                $incentive->setParticipantId($participantId);
+                $incentive->setCreatedTs($now);
+                $incentive->setUser($this->userService->getUserEntity());
+                $incentive->setSite($this->siteService->getSiteId());
+                $this->em->persist($incentive);
+                $this->em->flush();
+                $this->loggerService->log(Log::INCENTIVE_ADD, $incentive->getId());
+            }
+        } catch (\Exception $e) {
+            $this->rdrApiService->logException($e);
+            return false;
+        }
+        return false;
+    }
+
+    public function amendIncentive($participantId, $incentiveForm)
+    {
+        $incentive = $incentiveForm->getData();
+        if ($incentive->getIncentiveAmount() === 'other') {
+            $incentive->setIncentiveAmount($incentiveForm['other_incentive_amount']->getData());
+        }
+        $postData = $this->getRdrObject($incentive, 'amend');
+        try {
+            $response = $this->rdrApiService->put("rdr/v1/Participant/{$participantId}/Incentives", $postData);
+            $result = json_decode($response->getBody()->getContents());
+            if (is_object($result) && isset($result->authored)) {
+                $now = new \DateTime();
+                $incentive->setAmendedTs($now);
+                $incentive->setAmendedUser($this->userService->getUserEntity());
+                $this->em->persist($incentive);
+                $this->em->flush();
+                $this->loggerService->log(Log::INCENTIVE_EDIT, $incentive->getId());
+            }
+        } catch (\Exception $e) {
+            $this->rdrApiService->logException($e);
+            return false;
+        }
+        return false;
+    }
+
+    public function cancelIncentive($participantId, $incentiveForm)
+    {
+        $incentive = $incentiveForm->getData();
+        if ($incentive->getIncentiveAmount() === 'other') {
+            $incentive->setIncentiveAmount($incentiveForm['other_incentive_amount']->getData());
+        }
+        $postData = $this->getRdrObject($incentive, 'cancel');
+        try {
+            $response = $this->rdrApiService->put("rdr/v1/Participant/{$participantId}/Incentives", $postData);
+            $result = json_decode($response->getBody()->getContents());
+            if (is_object($result) && isset($result->authored)) {
+                $now = new \DateTime();
+                $incentive->setCancelledTs($now);
+                $incentive->setCancelledUser($this->userService->getUserEntity());
+                $this->em->persist($incentive);
+                $this->em->flush();
+                $this->loggerService->log(Log::INCENTIVE_REMOVE, $incentive->getId());
+            }
+        } catch (\Exception $e) {
+            $this->rdrApiService->logException($e);
+            return false;
+        }
+        return false;
+    }
+}
