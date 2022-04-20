7366f685d2e0d9893abe58657a9b5f0a2781055c
Shyam Naru <shyam.s.naru@vumc.org>
2022-04-20 13:47:53 -0500

    Write test case for getIdVerifications method

--- tests/Service/IdVerificationServiceTest.php
+++ tests/Service/IdVerificationServiceTest.php
@@ -7,8 +7,7 @@ use App\Service\LoggerService;
 use App\Service\RdrApiService;
 use App\Service\SiteService;
 use App\Service\UserService;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Psr7\Utils;
+use GuzzleHttp\Psr7\Response;
 
 class IdVerificationServiceTest extends ServiceTestCase
 {
@@ -39,6 +38,37 @@ class IdVerificationServiceTest extends ServiceTestCase
         self::assertTrue($result);
     }
 
+    public function testGetIdVerifications(): void
+    {
+        $result = $this->service->getIdVerifications('P123456789');
+        self::assertEquals('P123456789', $result[0]->participantId);
+        self::assertEquals('2022-04-19T20:52:23', $result[0]->verifiedTime);
+        self::assertEquals('test@example.com', $result[0]->userEmail);
+        self::assertEquals('hpo-site-test', $result[0]->siteGoogleGroup);
+        self::assertEquals('PHOTO_AND_ONE_OF_PII', $result[0]->verificationType);
+        self::assertEquals('PMB_INITIAL_VISIT', $result[0]->visitType);
+    }
+
+    private function getIdVerificationService(): IdVerificationService
+    {
+        $mockRdrApiService = $this->createMock(RdrApiService::class);
+        $createResponseData = $this->getCreateMockResponseData();
+        $idVerificationsData = $this->getIdVerificationsData();
+        $mockRdrApiService->method('post')->willReturn($this->getGuzzleResponse($createResponseData));
+        $mockRdrApiService->method('get')->willReturn($this->getGuzzleResponse($idVerificationsData));
+        return new IdVerificationService(
+            $mockRdrApiService,
+            static::getContainer()->get(SiteService::class),
+            static::getContainer()->get(UserService::class),
+            $this->createMock(LoggerService::class)
+        );
+    }
+
+    private function getGuzzleResponse($data): Response
+    {
+        return new Response(200, ['Content-Type' => 'application/json'], $data);
+    }
+
     private function getIdVerificationData(): array
     {
         $idVerificationData = [
@@ -53,17 +83,8 @@ class IdVerificationServiceTest extends ServiceTestCase
         return '{"participantId" : "P123456789", "verificationType": "PHOTO_AND_ONE_OF_PII"}';
     }
 
-    private function getIdVerificationService(): IdVerificationService
+    private function getIdVerificationsData(): string
     {
-        $mockRdrApiService = $this->createMock(RdrApiService::class);
-        $stream = Utils::streamFor($this->getCreateMockResponseData());
-        $response = new Psr7\Response(200, ['Content-Type' => 'application/json'], $stream);
-        $mockRdrApiService->method('post')->willReturn($response);
-        return new IdVerificationService(
-            $mockRdrApiService,
-            static::getContainer()->get(SiteService::class),
-            static::getContainer()->get(UserService::class),
-            $this->createMock(LoggerService::class)
-        );
+        return '{"entry": [{"participantId": "P123456789", "verifiedTime": "2022-04-19T20:52:23", "userEmail": "test@example.com", "siteGoogleGroup": "hpo-site-test", "siteName": "Test", "verificationType": "PHOTO_AND_ONE_OF_PII", "visitType": "PMB_INITIAL_VISIT"}]}';
     }
 }
