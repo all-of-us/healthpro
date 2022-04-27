1dc33ed31fc01df2e41c620b6ac54c440418090e
Shyam Naru <shyam.s.naru@vumc.org>
2022-04-27 11:47:48 -0500

    Write test case for order entity timestamps

--- tests/Entity/OrderTest.php
+++ tests/Entity/OrderTest.php
@@ -412,13 +412,13 @@ class OrderTest extends TestCase
     }
 
     /**
-     * @dataProvider biobankOrderDataProvider
+     * @dataProvider getBiobankChangesDataProvider
      */
-    public function testBiobankChanges($data, $json)
+    public function testGetBiobankChanges($data, $json)
     {
         $createdTs = new \DateTime('2022-01-01 08:00:00');
         $finalizedTs = new \DateTime('2022-01-02 08:00:00');
-        $finalizedSamples = ["1SS08","1PS08","1HEP4","1ED04","1ED10","1CFD9","1PXR2","1UR10"];
+        $finalizedSamples = ["1SS08", "1PS08", "1HEP4", "1ED04", "1ED10", "1CFD9", "1PXR2", "1UR10"];
         $centrifugeType = 'swinging_bucket';
         $orderData = [
             'user' => $this->getUser(),
@@ -433,6 +433,33 @@ class OrderTest extends TestCase
         self::assertEquals($order->getBiobankChanges(), $json);
     }
 
+    /**
+     * @dataProvider biobankOrderDataProvider
+     */
+    public function testBiobankOrderChanges($data, $result)
+    {
+        $createdTs = new \DateTime('2022-01-01 08:00:00');
+        $finalizedTs = new \DateTime('2022-01-02 08:00:00');
+        $finalizedSamples = ["1SS08", "1PS08", "1HEP4", "1ED04", "1ED10", "1CFD9", "1PXR2", "1UR10"];
+        $centrifugeType = 'swinging_bucket';
+        $orderData = [
+            'user' => $this->getUser(),
+            'site' => 'test',
+            'createdTs' => $createdTs,
+            'participantId' => 'P123456789'
+        ];
+        $collectedTs = $data['collectedTs'];
+        $orderData = array_merge($orderData, $data);
+        $order = $this->createOrder($orderData);
+        if (empty($collectedTs)) {
+            $order->setCollectedTs($createdTs);
+        }
+        $order->checkBiobankChanges($collectedTs, $finalizedTs, $finalizedSamples, '', $centrifugeType);
+        self::assertEquals($order->getCollectedTs(), $result['collectedTs']);
+        self::assertEquals($order->getProcessedTs(), $result['processedTs']);
+        self::assertEquals($order->getFinalizedTs(), $finalizedTs);
+    }
+
     public function orderTypeProvider(): array
     {
         return [
@@ -445,9 +472,9 @@ class OrderTest extends TestCase
         ];
     }
 
-    public function biobankOrderDataProvider(): array
+    public function getBiobankChangesDataProvider(): array
     {
-        $ts = new \DateTime('2022-01-01 08:00:00');
+        $ts = new \DateTime('2022-01-03 08:00:00');
         return [
             [
                 [
@@ -478,6 +505,56 @@ class OrderTest extends TestCase
                     'processedSamplesTs' => '{"1SS08":1641024000,"1PS08":1641024000}'
                 ],
                 '{"collected":{"site":"test","samples":["1SS08","1PS08","1HEP4","1ED04","1ED10","1CFD9","1PXR2","1UR10"]},"processed":{"site":"test","samples":["1SS08","1PS08"],"samples_ts":{"1SS08":1641024000,"1PS08":1641024000},"centrifuge_type":"swinging_bucket"},"finalized":{"time":1641110400,"site":"test","user":null,"notes":"","samples":["1SS08","1PS08","1HEP4","1ED04","1ED10","1CFD9","1PXR2","1UR10"]}}'
+            ],
+        ];
+    }
+
+    public function biobankOrderDataProvider(): array
+    {
+        $createdTs = new \DateTime('2022-01-01 08:00:00');
+        $ts = new \DateTime('2022-01-03 08:00:00');
+        return [
+            [
+                [
+                    'collectedTs' => null,
+                    'processedTs' => null,
+                ],
+                [
+                    'collectedTs' => $createdTs,
+                    'processedTs' => $createdTs,
+                ],
+            ],
+            [
+                [
+                    'collectedTs' => $ts,
+                    'processedTs' => null,
+                ],
+                [
+                    'collectedTs' => $ts,
+                    'processedTs' => $createdTs,
+                ],
+            ],
+            [
+                [
+                    'collectedTs' => null,
+                    'processedTs' => $ts,
+                    'processedSamplesTs' => '{"1SS08":1641024000,"1PS08":1641024000}'
+                ],
+                [
+                    'collectedTs' => $createdTs,
+                    'processedTs' => $ts,
+                ],
+            ],
+            [
+                [
+                    'collectedTs' => $ts,
+                    'processedTs' => $ts,
+                    'processedSamplesTs' => '{"1SS08":1641024000,"1PS08":1641024000}'
+                ],
+                [
+                    'collectedTs' => $ts,
+                    'processedTs' => $ts,
+                ],
             ],
         ];
     }
