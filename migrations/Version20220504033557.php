f3affba5a6a0a7fe8a93b5e86347657df7b31130
Shyam Naru <shyam.s.naru@vumc.org>
2022-05-12 16:19:52 -0500

    Remove auto generated code form migrations

--- migrations/Version20220504033557.php
+++ migrations/Version20220504033557.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220504033557 extends AbstractMigration
 {
     public function getDescription() : string
