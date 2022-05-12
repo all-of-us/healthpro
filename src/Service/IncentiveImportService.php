c37185703119d00c1dc576ec21c2f6c79f5c4b06
Shyam Naru <shyam.s.naru@vumc.org>
2022-05-12 13:24:51 -0500

    Validate CSV form fields

--- src/Service/IncentiveImportService.php
+++ src/Service/IncentiveImportService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Entity\Incentive;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\FormError;
@@ -48,6 +49,27 @@ class IncentiveImportService
             }
             if (!$this->isValidEmail($data[1])) {
                 $form['incentive_csv']->addError(new FormError("Invalid email address {$data[1]} in line {$row}, column 2"));
+            }
+            if (!in_array($data[3], array_values(Incentive::$incentiveOccurrenceChoices))) {
+                $form['incentive_csv']->addError(new FormError("Invalid incentive occurrence {$data[3]} in line {$row}, column 4"));
+            }
+            if (!in_array($data[5], array_values(Incentive::$incentiveTypeChoices))) {
+                $form['incentive_csv']->addError(new FormError("Invalid incentive type {$data[5]} in line {$row}, column 6"));
+            }
+            if (!in_array($data[8], array_values(Incentive::$incentiveAmountChoices))) {
+                $form['incentive_csv']->addError(new FormError("Invalid incentive amount {$data[8]} in line {$row}, column 9"));
+            }
+            if ($data[3] === 'other' && empty($data[4])) {
+                $form['incentive_csv']->addError(new FormError("Please enter other incentive occurrence in line {$row}, column 5"));
+            }
+            if ($data[5] === 'other' && empty($data[7])) {
+                $form['incentive_csv']->addError(new FormError("Please enter other incentive type in line {$row}, column 8"));
+            }
+            if ($data[5] === 'gift_card' && empty($data[6])) {
+                $form['incentive_csv']->addError(new FormError("Please enter gift card type in line {$row}, column 7"));
+            }
+            if ($data[8] === 'other' && empty($data[9])) {
+                $form['incentive_csv']->addError(new FormError("Please enter other incentive amount in line {$row}, column 10"));
             }
             $incentive['participant_id'] = $data[0];
             $incentive['user_email'] = $data[1];
