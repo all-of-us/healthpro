aee93dfcab2a3f7f287f055bf1d3e614c5cdfc3d
Shyam <shyamnaru@gmail.com>
2022-06-13 15:14:12 -0500

    Create ID verification details page

--- src/Entity/IdVerificationImport.php
+++ src/Entity/IdVerificationImport.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -10,6 +11,11 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class IdVerificationImport
 {
+    public function __construct()
+    {
+        $this->idVerificationImportRows = new ArrayCollection();
+    }
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
