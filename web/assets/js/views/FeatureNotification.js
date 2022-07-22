6b2a144f95a0190733bb0d5658b77b5d0befd0a4
Shyam <shyamnaru@gmail.com>
2022-07-22 16:52:46 -0500

    Remove url paterns

--- web/assets/js/views/FeatureNotification.js
+++ web/assets/js/views/FeatureNotification.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
     $('#feature_notification_start_ts, #feature_notification_end_ts').pmiDateTimePicker();
     $('#feature_notification_url').dropdownOther({
-        'All': '/*',
         'Home Page': '/',
         'Work Queue': '/workqueue/',
-        'Biobank Order Pages': '/participant/*/order/*',
-        'Physical Measurements': '/participant/*/measurements*',
         'Management Tools': '/access/manage/dashboard',
         'User Management': '/access/manage/user/groups'
     });
