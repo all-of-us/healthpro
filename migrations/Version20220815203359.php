4223597a94af18a515ace6f01a8fc22f60143220
Shyam <shyamnaru@gmail.com>
2022-08-15 16:03:28 -0500

    Create migration for WorkqueueView Entity

---
+++ migrations/Version20220815203359.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220815203359 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create WorkqueueView Entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('CREATE TABLE workqueue_view (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, site VARCHAR(255) DEFAULT NULL, awardee VARCHAR(255) DEFAULT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(50) NOT NULL, default_view TINYINT(1) NOT NULL, created_ts DATETIME NOT NULL, INDEX IDX_7439EA30A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('DROP TABLE workqueue_view');
+    }
+}
