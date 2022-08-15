2dab1edb3375fc02c0aa76649a6cd600d6664adb
Shyam <shyamnaru@gmail.com>
2022-08-15 15:32:49 -0500

    Create WorkqueueView entity

---
+++ src/Entity/WorkqueueView.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class WorkqueueView
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $site;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $awardee;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $defaultView;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdTs;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getSite(): ?string
+    {
+        return $this->site;
+    }
+
+    public function setSite(string $site): self
+    {
+        $this->site = $site;
+
+        return $this;
+    }
+
+    public function getAwardee(): ?string
+    {
+        return $this->awardee;
+    }
+
+    public function setAwardee(?string $awardee): self
+    {
+        $this->awardee = $awardee;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDefaultView(): ?bool
+    {
+        return $this->defaultView;
+    }
+
+    public function setDefaultView(bool $defaultView): self
+    {
+        $this->defaultView = $defaultView;
+
+        return $this;
+    }
+
+    public function getCreatedTs(): ?\DateTimeInterface
+    {
+        return $this->createdTs;
+    }
+
+    public function setCreatedTs(\DateTimeInterface $createdTs): self
+    {
+        $this->createdTs = $createdTs;
+
+        return $this;
+    }
+}
