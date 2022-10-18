e427d55765e97cecd197901b143a38cacee48136
Andrew Johnson <666617+Ehaic@users.noreply.github.com>
2022-10-18 12:53:20 -0500

    Small loader for program specific templates

---
+++ src/Service/ContextTemplateService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Service;
+use Symfony\Component\HttpFoundation\Session;
+use Twig\Extension\RuntimeExtensionInterface;
+
+
+class ContextTemplateService implements RuntimeExtensionInterface {
+    private Session\SessionInterface $session;
+    public function __construct(Session\SessionInterface $session) {
+        $this->session = $session;
+    }
+    public function GetProgramTemplate(string $RelativePath): string {
+        return 'program/' . $this->session->get('program', 'hpo') . '/' . $RelativePath;
+    }
+}
