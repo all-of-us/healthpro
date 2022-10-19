983a330fdc4358d97618d02e3703ffe39286b056
Andrew Johnson <andrew.c.johnson@vumc.org>
2022-10-19 11:24:50 -0500

    fixed Context Template Service not meeting php-cs-fixer rules.

--- src/Service/ContextTemplateService.php
+++ src/Service/ContextTemplateService.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Service;
+
 use Symfony\Component\HttpFoundation\Session;
 use Twig\Extension\RuntimeExtensionInterface;
 
-
-class ContextTemplateService implements RuntimeExtensionInterface {
+class ContextTemplateService implements RuntimeExtensionInterface
+{
     private Session\SessionInterface $session;
-    public function __construct(Session\SessionInterface $session) {
+    public function __construct(Session\SessionInterface $session)
+    {
         $this->session = $session;
     }
-    public function GetProgramTemplate(string $RelativePath): string {
+    public function GetProgramTemplate(string $RelativePath): string
+    {
         return 'program/' . $this->session->get('program', 'hpo') . '/' . $RelativePath;
     }
 }
