ff880cafcccc3d5a89c7d05189bd6ddc6adfbc5a
Shyam <shyamnaru@gmail.com>
2022-11-02 16:29:05 -0500

    Create entities for NPH order workflow

---
+++ src/Entity/NphAliquot.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NphAliquotRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NphAliquotRepository::class)
+ */
+class NphAliquot
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $aliquotId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=NphSample::class, inversedBy="nphAliquots")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $nphSample;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $aliquotTs;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAliquotId(): ?string
+    {
+        return $this->aliquotId;
+    }
+
+    public function setAliquotId(string $aliquotId): self
+    {
+        $this->aliquotId = $aliquotId;
+
+        return $this;
+    }
+
+    public function getNphSample(): ?NphSample
+    {
+        return $this->nphSample;
+    }
+
+    public function setNphSample(?NphSample $nphSample): self
+    {
+        $this->nphSample = $nphSample;
+
+        return $this;
+    }
+
+    public function getAliquotTs(): ?\DateTimeInterface
+    {
+        return $this->aliquotTs;
+    }
+
+    public function setAliquotTs(\DateTimeInterface $aliquotTs): self
+    {
+        $this->aliquotTs = $aliquotTs;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
