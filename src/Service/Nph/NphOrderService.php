92e51c35356626e7a1a11e9bd44188c25f8ebe3b
Shyam <shyamnaru@gmail.com>
2022-11-04 14:34:56 -0500

    Create generate order & sample id methods

--- src/Service/Nph/NphOrderService.php
+++ src/Service/Nph/NphOrderService.php
@@ -2,11 +2,22 @@
 
 namespace App\Service\Nph;
 
+use App\Entity\NphOrder;
+use App\Entity\NphSample;
+use Doctrine\ORM\EntityManagerInterface;
+
 class NphOrderService
 {
     public $module;
 
     public $visit;
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
 
     public function loadModules($module, $visit): void
     {
@@ -25,5 +36,52 @@ class NphOrderService
     public function getTimePoints()
     {
         return $this->visit->timePoints;
+    }
+
+    public function generateOrderId(): string
+    {
+        $attempts = 0;
+        $nphOrderRepository = $this->em->getRepository(NphOrder::class);
+        while (++$attempts <= 20) {
+            $id = $this->getNumericId();
+            if ($nphOrderRepository->findOneBy(['orderId' => $id])) {
+                $id = null;
+            } else {
+                break;
+            }
+        }
+        if (empty($id)) {
+            throw new \Exception('Failed to generate unique order id');
+        }
+        return $id;
+    }
+
+    public function generateSampleId(): string
+    {
+        $attempts = 0;
+        $nphSampleRepository = $this->em->getRepository(NphSample::class);
+        while (++$attempts <= 20) {
+            $id = $this->getNumericId();
+            if ($nphSampleRepository->findOneBy(['sampleId' => $id])) {
+                $id = null;
+            } else {
+                break;
+            }
+        }
+        if (empty($id)) {
+            throw new \Exception('Failed to generate unique sample id');
+        }
+        return $id;
+    }
+
+    private function getNumericId(): string
+    {
+        $length = 10;
+        // Avoid leading 0s
+        $id = (string)rand(1, 9);
+        for ($i = 0; $i < $length - 1; $i++) {
+            $id .= (string)rand(0, 9);
+        }
+        return $id;
     }
 }
