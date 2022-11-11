d995d4472a768fff50204fca0a65fb4a08642c0b
Shyam <shyamnaru@gmail.com>
2022-11-11 11:53:52 -0600

    Disable samples if already exists

--- src/Form/Nph/NphOrderType.php
+++ src/Form/Nph/NphOrderType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form\Nph;
 
-use App\Nph\Order\Samples;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type;
@@ -13,6 +12,7 @@ class NphOrderType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $ordersData = $builder->getData();
         $timePointSamples = $options['timePointSamples'];
         $timePoints = $options['timePoints'];
         foreach ($timePointSamples as $timePoint => $samples) {
@@ -21,6 +21,7 @@ class NphOrderType extends AbstractType
                     $builder->add('stoolKit', Type\TextType::class, [
                         'label' => 'Stool Kit ID',
                         'required' => false,
+                        'disabled' => !empty($ordersData['stoolKit']),
                         'constraints' => new Constraints\Type('string'),
                         'attr' => [
                             'placeholder' => 'Scan Kit ID'
@@ -31,6 +32,7 @@ class NphOrderType extends AbstractType
                     $builder->add($sampleCode, Type\TextType::class, [
                         'label' => $sample,
                         'required' => false,
+                        'disabled' => !empty($ordersData[$sampleCode]),
                         'constraints' => new Constraints\Type('string'),
                         'attr' => [
                             'placeholder' => 'Scan Tube'
@@ -44,7 +46,15 @@ class NphOrderType extends AbstractType
                 'multiple' => true,
                 'label' => $timePoints[$timePoint],
                 'choices' => array_flip($samples),
-                'required' => false
+                'required' => false,
+                'choice_attr' => function ($val) use ($ordersData, $timePoint) {
+                    $attr = [];
+                    if (isset($ordersData[$timePoint]) && in_array($val, $ordersData[$timePoint])) {
+                        $attr['disabled'] = true;
+                        $attr['class'] = 'sample-disabled';
+                    }
+                    return $attr;
+                }
             ]);
         }
         return $builder->getForm();
