8c4af94ec78619124f0e052092410ade48a85c9c
Shyam <shyamnaru@gmail.com>
2022-11-16 17:04:02 -0600

    Write test cases for module 1 samples & timepoints

---
+++ tests/Service/NphOrderServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\Nph\NphOrderService;
+use App\Service\SiteService;
+
+class NphOrderServiceTest extends ServiceTestCase
+{
+    protected $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->login('test@example.com', ['nph-site-test'], 'America/Chicago');
+        $siteService = static::$container->get(SiteService::class);
+        $siteService->switchSite('nph-site-test' . '@' . self::GROUP_DOMAIN);
+        $this->service = static::$container->get(NphOrderService::class);
+    }
+
+    public function testLoadModules(): void
+    {
+        // Module 1
+        $module1Data = json_decode(file_get_contents(__DIR__ . '/data/order_module_1.json'), true);
+        $this->service->loadModules(1, 'LMT', 'P0000000001');
+        $this->assertSame($module1Data['timePointSamples'], $this->service->getTimePointSamples());
+        $this->assertSame($module1Data['timePoints'], $this->service->getTimePoints());
+        $this->assertSame($module1Data['samples'], $this->service->getSamples());
+        $this->assertSame($module1Data['nailSamples'], $this->service->getNailSamples());
+        $this->assertSame($module1Data['stoolSamples'], $this->service->getStoolSamples());
+    }
+}
