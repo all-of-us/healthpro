ca3c612397c7f835db29041c8070a8bdda1a7c50
Shyam <shyamnaru@gmail.com>
2022-11-17 13:41:45 -0600

    Write test case for create order

--- tests/Service/NphOrderServiceTest.php
+++ tests/Service/NphOrderServiceTest.php
@@ -4,18 +4,22 @@ namespace App\Tests\Service;
 
 use App\Service\Nph\NphOrderService;
 use App\Service\SiteService;
+use Doctrine\ORM\EntityManagerInterface;
 
 class NphOrderServiceTest extends ServiceTestCase
 {
     protected $service;
+    protected $em;
 
     public function setUp(): void
     {
         parent::setUp();
+        $this->program = 'nph';
         $this->login('test@example.com', ['nph-site-test'], 'America/Chicago');
         $siteService = static::$container->get(SiteService::class);
         $siteService->switchSite('nph-site-test' . '@' . self::GROUP_DOMAIN);
         $this->service = static::$container->get(NphOrderService::class);
+        $this->em = static::$container->get(EntityManagerInterface::class);
     }
 
     public function testLoadModules(): void
@@ -37,6 +41,7 @@ class NphOrderServiceTest extends ServiceTestCase
      */
     public function testGetSampleType($sampleType, $sampleCode): void
     {
+        // Module 1
         $this->service->loadModules(1, 'LMT', 'P0000000001');
         $this->assertSame($sampleType, $this->service->getSampleType($sampleCode));
     }
@@ -50,5 +55,15 @@ class NphOrderServiceTest extends ServiceTestCase
             ['urine', 'URINES'],
             ['saliva', 'SALIVA']
         ];
+    }
+
+    public function testCreateOrder()
+    {
+        // Module 1
+        $this->service->loadModules(1, 'LMT', 'P0000000001');
+        $nphOrder = $this->service->createOrder('preLMT', 'saliva');
+        $this->assertSame('LMT', $nphOrder->getVisitType());
+        $this->assertSame('preLMT', $nphOrder->getTimepoint());
+        $this->assertSame('saliva', $nphOrder->getOrderType());
     }
 }
