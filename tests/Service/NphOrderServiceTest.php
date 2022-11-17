9dde100cab06088337c602d951f52e11bb3eb864
Shyam <shyamnaru@gmail.com>
2022-11-17 15:37:13 -0600

    Write tests for create orders & samples

--- tests/Service/NphOrderServiceTest.php
+++ tests/Service/NphOrderServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Service;
 
+use App\Entity\NphSample;
 use App\Service\Nph\NphOrderService;
 use App\Service\SiteService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,6 +11,7 @@ class NphOrderServiceTest extends ServiceTestCase
 {
     protected $service;
     protected $em;
+    protected $module1Data;
 
     public function setUp(): void
     {
@@ -20,20 +22,21 @@ class NphOrderServiceTest extends ServiceTestCase
         $siteService->switchSite('nph-site-test' . '@' . self::GROUP_DOMAIN);
         $this->service = static::$container->get(NphOrderService::class);
         $this->em = static::$container->get(EntityManagerInterface::class);
+        // Module 1
+        $this->module1Data = json_decode(file_get_contents(__DIR__ . '/data/order_module_1.json'), true);
     }
 
     public function testLoadModules(): void
     {
         // Module 1
-        $module1Data = json_decode(file_get_contents(__DIR__ . '/data/order_module_1.json'), true);
         $this->service->loadModules(1, 'LMT', 'P0000000001');
-        $this->assertSame($module1Data['timePointSamples'], $this->service->getTimePointSamples());
-        $this->assertSame($module1Data['timePoints'], $this->service->getTimePoints());
-        $this->assertSame($module1Data['samples'], $this->service->getSamples());
+        $this->assertSame($this->module1Data['timePointSamples'], $this->service->getTimePointSamples());
+        $this->assertSame($this->module1Data['timePoints'], $this->service->getTimePoints());
+        $this->assertSame($this->module1Data['samples'], $this->service->getSamples());
 
-        $this->assertSame($module1Data['stoolSamples'], $this->service->getSamplesByType('stool'));
-        $this->assertSame($module1Data['bloodSamples'], $this->service->getSamplesByType('blood'));
-        $this->assertSame($module1Data['nailSamples'], $this->service->getSamplesByType('nail'));
+        $this->assertSame($this->module1Data['stoolSamples'], $this->service->getSamplesByType('stool'));
+        $this->assertSame($this->module1Data['bloodSamples'], $this->service->getSamplesByType('blood'));
+        $this->assertSame($this->module1Data['nailSamples'], $this->service->getSamplesByType('nail'));
     }
 
     /**
@@ -42,7 +45,7 @@ class NphOrderServiceTest extends ServiceTestCase
     public function testGetSampleType($sampleType, $sampleCode): void
     {
         // Module 1
-        $this->service->loadModules(1, 'LMT', 'P0000000001');
+        $this->service->loadModules(1, 'LMT', 'P0000000002');
         $this->assertSame($sampleType, $this->service->getSampleType($sampleCode));
     }
 
@@ -60,10 +63,29 @@ class NphOrderServiceTest extends ServiceTestCase
     public function testCreateOrder()
     {
         // Module 1
-        $this->service->loadModules(1, 'LMT', 'P0000000001');
+        $this->service->loadModules(1, 'LMT', 'P0000000003');
         $nphOrder = $this->service->createOrder('preLMT', 'saliva');
         $this->assertSame('LMT', $nphOrder->getVisitType());
         $this->assertSame('preLMT', $nphOrder->getTimepoint());
         $this->assertSame('saliva', $nphOrder->getOrderType());
+    }
+
+    public function testCreateSample()
+    {
+        // Module 1
+        $this->service->loadModules(1, 'LMT', 'P0000000004');
+        $nphOrder = $this->service->createOrder('preLMT', 'saliva');
+        $this->service->createSample('SALIVA', $nphOrder);
+
+        $nphSample = $this->em->getRepository(NphSample::class)->findOneBy(['nphOrder' => $nphOrder]);
+        $this->assertSame('SALIVA', $nphSample->getSampleCode());
+    }
+
+    public function testCreateOrdersAndSamples()
+    {
+        // Module 1
+        $this->service->loadModules(1, 'LMT', 'P0000000005');
+        $this->service->createOrdersAndSamples($this->module1Data['formData']);
+        $this->assertSame($this->module1Data['formData'], $this->service->getExistingOrdersData());
     }
 }
