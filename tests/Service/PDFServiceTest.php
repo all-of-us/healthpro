984e6b549175fb3ebe40f3d76b245d07a2a8fb11
Andrew Johnson <andrew.c.johnson@vumc.org>
2022-12-02 09:01:16 -0600

    Updated Test, added object generation methods instead of using array

--- tests/Service/PDFServiceTest.php
+++ tests/Service/PDFServiceTest.php
@@ -2,38 +2,35 @@
 
 namespace App\Tests\Service;
 
-use App\Helper\Participant;
+use App\Entity\NphOrder;
+use App\Entity\NphSample;
+use App\Service\Nph\NphOrderService;
 use App\Service\PDFService;
+use App\Service\SiteService;
 use App\Tests\testSetup;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PDFServiceTest extends ServiceTestCase
 {
 
-    protected function getOrderData(): array
+    protected $service;
+    protected $testSetup;
+    protected NphOrderService $nphOrderService;
+
+    public function setUp(): void
     {
-        return [
-            'minus15min' => [
-                    '6505378640' => [
-                        'SST8P5' => [
-                            'SampleID' => '9292338307',
-                            'SampleName' => '8.5 mL SST',
-                            'OrderID' => '6505378640',
-                            'SampleCollectionVolume' => '8.5 mL',
-                            'TimepointDisplayName' => '-15 min',
-                        ]
-                    ]
-                ]
-        ];
+        $this->service = static::getContainer()->get(PDFService::class);
+        $this->testSetup = new testSetup(static::getContainer()->get(EntityManagerInterface::class));
+        $this->nphOrderService = static::getContainer()->get(NphOrderService::class);
     }
 
     public function testBatchPDF(): void
     {
-        $module = '1';
-        $visit = 'LMT';
         $pdfService = static::getContainer()->get(PDFService::class);
-        $participant = testSetup::generateParticipant('P000001', 'John', 'Doe', new \DateTime('2000-01-01'));
-        $orderSummary = $this->getOrderData();
-        $pdf = $pdfService->batchPDF($orderSummary, $participant, $module, $visit);
+        $participant = $this->testSetup->generateParticipant();
+        $nphOrder = $this->testSetup->generateNphOrder($participant);
+        $orderSummary = $this->nphOrderService->getParticipantOrderSummaryByModuleAndVisit($participant->id, $nphOrder->getModule(), $nphOrder->getVisitType());
+        $pdf = $pdfService->batchPDF($orderSummary['order'], $participant, $nphOrder->getModule(), $nphOrder->getVisitType());
         $this->assertIsString($pdf);
         $this->assertStringContainsString('%PDF', $pdf);
     }
--- tests/testSetup.php
+++ tests/testSetup.php
@@ -2,11 +2,24 @@
 
 namespace App\Tests;
 
+use App\Entity\NphOrder;
+use App\Entity\NphSample;
+use App\Entity\NphSite;
+use App\Entity\Site;
+use App\Entity\User;
 use App\Helper\Participant;
+use Doctrine\ORM\EntityManagerInterface;
 
 class testSetup
 {
-    public static function generateParticipant(string $id = null, string $firstName = null, string $lastName = null, \DateTime $dateOfBirth = null): Participant
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function generateParticipant(string $id = null, string $firstName = null, string $lastName = null, \DateTime $dateOfBirth = null): Participant
     {
         if ($id === null) {
             $id = "P0000001";
@@ -20,11 +33,75 @@ class testSetup
         if ($dateOfBirth === null) {
             $dateOfBirth = new \DateTime('2000-01-01');
         }
-        return new Participant((object)[
-            'id' => $id,
+        $participant = new Participant((object)[
+            'participantId' => $id,
             'dateOfBirth' => $dateOfBirth->format('y-m-d'),
             'firstName' => $firstName,
             'lastName' => $lastName
         ]);
+        return $participant;
+
+    }
+
+    public function generateNPHOrder(Participant $participant): NphOrder
+    {
+        $user = $this->generateUser();
+        $siteId = $this->generateSite('hpo')->getGoogleGroup();
+        $nphOrder = new NphOrder();
+        $nphOrder->setModule(1);
+        $nphOrder->setVisitType('LMT');
+        $nphOrder->setTimepoint('preLMT');
+        $nphOrder->setOrderId('100000001');
+        $nphOrder->setParticipantId($participant->id);
+        $nphOrder->setUser($user);
+        $nphOrder->setSite($siteId);
+        $nphOrder->setCreatedTs(new \DateTime());
+        $nphOrder->setOrderType('urine');
+        $this->em->persist($nphOrder);
+        $this->em->flush();
+
+        $nphSample = new NphSample();
+        $nphSample->setNphOrder($nphOrder);
+        $nphSample->setSampleId('100000002');
+        $nphSample->setSampleCode('URINES');
+        $this->em->persist($nphSample);
+        $this->em->flush();
+        return $nphOrder;
+    }
+
+    public function generateUser(): User
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setGoogleId('12345');
+        $this->em->persist($user);
+        $this->em->flush();
+        return $user;
+    }
+
+
+    public function generateSite($type = 'hpo'): Site
+    {
+        $id = uniqid();
+        $orgId = 'TEST_ORG_' . $id;
+        $siteId = 'test-' . $id;
+        if ($type === 'nph') {
+            $site = new NphSite();
+            $site->setStatus(true)
+                ->setName('Test Site ' . $id)
+                ->setOrganizationId($orgId)
+                ->setGoogleGroup($siteId);
+        } else {
+            $site = new Site();
+            $site->setStatus(true)
+                ->setName('Test Site ' . $id)
+                ->setOrganizationId($orgId)
+                ->setSiteId($siteId)
+                ->setGoogleGroup($siteId)
+                ->setWorkqueueDownload('');
+        }
+        $this->em->persist($site);
+        $this->em->flush();
+        return $site;
     }
 }
