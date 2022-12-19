3fe9f4f405a9b1bb9140b74253a81cdbfce7921e
Shyam <shyamnaru@gmail.com>
2022-12-19 14:00:39 -0600

    Create migration for modify reason and type properties

---
+++ migrations/Version20221219195323.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221219195323 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Adds modifyReason and modifyType properties to NphOrder entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE nph_orders ADD modify_reason LONGTEXT DEFAULT NULL, ADD modify_type VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE nph_orders DROP modify_reason, DROP modify_type');
+    }
+}
