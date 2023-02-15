d7ae862b24f6fac9a2ee17e837ccb3e256b22908
Andrew Johnson <andrew.c.johnson@vumc.org>
2023-02-15 14:33:42 -0600

    Missed auto generated comment.

--- migrations/Version20230210142836.php
+++ migrations/Version20230210142836.php
@@ -26,7 +26,6 @@ final class Version20230210142836 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('DROP TABLE nph_field_sort');
     }
