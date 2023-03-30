934ae5ad9919046ccaf91616d2932d2e24357a4a
Shyam <shyam.s.naru@vumc.org>
2023-03-30 11:25:42 -0500

    Write tests for getParticipantById and testGetAllParticipantDetailsById methods

---
+++ tests/Service/NphParticipantSummaryServiceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Helper\NphParticipant;
+use App\Service\Nph\NphParticipantSummaryService;
+use App\Service\RdrApiService;
+use GuzzleHttp\Psr7\Response;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class NphParticipantSummaryServiceTest extends ServiceTestCase
+{
+    public function testGetParticipantById()
+    {
+        $mockParamsService = $this->createMock(ParameterBagInterface::class);
+        $mockParamsService->method('has')->will($this->returnValueMap([
+            ['rdr_disable_cache', true]
+        ]));
+        $mockRdrApiService = $this->createMock(RdrApiService::class);
+        $data = $this->getMockRdrResponseData();
+        $mockRdrApiService->method('GQLPost')->willReturn($this->getGuzzleResponse($data));
+        $nphParticipantService = new NphParticipantSummaryService($mockRdrApiService, $mockParamsService);
+
+        // Test the getParticipantById method with a valid participant ID
+        $result = $nphParticipantService->getParticipantById('123456');
+        $this->assertInstanceOf(NphParticipant::class, $result);
+        $this->assertEquals('123456', $result->participantNphId);
+        $this->assertEquals('John', $result->firstName);
+
+        // Test the getParticipantById method with an invalid participant ID
+        $result = $nphParticipantService->getParticipantById('!@#$%^&*()');
+        $this->assertFalse($result);
+    }
+
+    public function testGetAllParticipantDetailsById()
+    {
+        $mockRdrApiService = $this->createMock(RdrApiService::class);
+        $data = $this->getMockRdrResponseData();
+        $mockRdrApiService->method('GQLPost')->willReturn($this->getGuzzleResponse($data));
+        $nphParticipantService = new NphParticipantSummaryService(
+            $mockRdrApiService,
+            static::getContainer()->get(ParameterBagInterface::class)
+        );
+        $result = $nphParticipantService->getAllParticipantDetailsById('123456');
+        $this->assertEquals('123456', $result['participantNphId']);
+        $this->assertEquals('John', $result['firstName']);
+    }
+
+    private function getGuzzleResponse($data): Response
+    {
+        return new Response(200, ['Content-Type' => 'application/json'], $data);
+    }
+
+    private function getMockRdrResponseData(): string
+    {
+        return '{"participant":{"edges":[{"node":{"DOB":"1990-01-01","biobankId":"T10000000011","firstName":"John","lastName":"Doe","nphPairedSite":"nph-site-test","participantNphId":"123456"}}],"resultCount":1,"totalCount":5}}';
+    }
+}
