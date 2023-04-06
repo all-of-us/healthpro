330c789dbcce94ccd1fd5fe8db71944b108561f7
Andrew Johnson <andrew.c.johnson@vumc.org>
2023-04-06 09:53:15 -0500

    Migration to update sort order for 180 timepoint.

---
+++ migrations/Version20230406141013.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230406141013 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('UPDATE nph_field_sort SET sort_order = sort_order + 1 WHERE field_value = \'240min\'');
+        $this->addSql('INSERT INTO nph_field_sort (field_value, sort_order) VALUES (\'180min\', 9)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('UPDATE nph_field_sort SET sort_order = 9 WHERE field_value = \'240min\'');
+        $this->addSql('DELETE FROM nph_field_sort WHERE field_value = \'180min\'');
+    }
+}
