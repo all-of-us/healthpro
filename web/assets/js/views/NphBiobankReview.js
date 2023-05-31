d585ac5158af8d161f310133e1ca5846d4cf9ae7
Shyam <shyam.s.naru@vumc.org>
2023-05-31 13:11:03 -0500

    Fix JS code style

--- web/assets/js/views/NphBiobankReview.js
+++ web/assets/js/views/NphBiobankReview.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
-    $('table').DataTable({
-        order: [[8, 'desc']],
+    $("table").DataTable({
+        order: [[8, "desc"]],
         pageLength: 25
     });
 
-    const dateTypes = [
-        "created_ts",
-        "collected_ts",
-        "finalized_ts"
-    ];
+    const dateTypes = ["created_ts", "collected_ts", "finalized_ts"];
 
     for (const dateType of dateTypes) {
         $("#" + dateType).html($("[data-date-type=" + dateType + "]").length);
