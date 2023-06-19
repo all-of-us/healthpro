0c7d6df31ea67ae211c460ec674b73a41db5b4d7
Shyam <shyam.s.naru@vumc.org>
2023-06-19 16:47:10 -0500

    Write tests for participant module

--- tests/Helper/NphParticipantTest.php
+++ tests/Helper/NphParticipantTest.php
@@ -16,4 +16,44 @@ class NphParticipantTest extends TestCase
         $this->assertSame('10000000000000', $participant->id);
         $this->assertSame('1999-05-20', $participant->dob->format('Y-m-d'));
     }
+
+    /**
+     * @dataProvider enrollmentStatusProvider
+     */
+    public function testParticipantModule($enrollmentStatus, $expectedModule): void
+    {
+        $participant = new NphParticipant((object)[
+            'nphEnrollmentStatus' => $enrollmentStatus
+        ]);
+        $this->assertSame($expectedModule, $participant->module);
+    }
+
+    public function enrollmentStatusProvider(): array
+    {
+        return [
+            'module1_complete' => [
+                [(object)['value' => 'module1_complete']],
+                1,
+            ],
+            'module2_consented' => [
+                [(object)['value' => 'module2_consented']],
+                2,
+            ],
+            'module3_eligibilityConfirmed' => [
+                [(object)['value' => 'module3_eligibilityConfirmed']],
+                3,
+            ],
+            'no_match' => [
+                [(object)['value' => 'invalid_status']],
+                null,
+            ],
+            'multiple_statuses_match' => [
+                [
+                    (object)['value' => 'module2_complete'],
+                    (object)['value' => 'module3_dietAssigned'],
+                ],
+                3,
+            ],
+        ];
+    }
 }
