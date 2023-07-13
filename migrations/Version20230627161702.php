5f4f214bf57181a6533b24702b0a18e595c11014
Andrew Johnson <andrew.c.johnson@vumc.org>
2023-07-13 13:34:42 -0500

    Removed generated comments, added description.

--- migrations/Version20230627161702.php
+++ migrations/Version20230627161702.php
@@ -7,26 +7,21 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230627161702 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Adds NPH DLW table and related fields for DLW extra information collection.';
     }
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('CREATE TABLE nph_dlw (id INT AUTO_INCREMENT NOT NULL, nph_participant VARCHAR(50) NOT NULL, module VARCHAR(10) NOT NULL, visit VARCHAR(50) NOT NULL, dose_batch_id VARCHAR(100) NOT NULL, actual_dose DOUBLE PRECISION NOT NULL, participant_weight DOUBLE PRECISION NOT NULL, dose_administered DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('DROP TABLE nph_dlw');
     }
