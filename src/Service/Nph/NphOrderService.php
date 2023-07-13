77f5b44d51f46fb57db31471078a1de6ae13f02b
Andrew Johnson <andrew.c.johnson@vumc.org>
2023-07-13 08:36:53 -0500

    WIP Commit

--- src/Service/Nph/NphOrderService.php
+++ src/Service/Nph/NphOrderService.php
@@ -755,6 +755,15 @@ class NphOrderService
         return $formErrors;
     }
 
+    public function saveDlwCollection(NphDlw $formData, $participantId, $module, $visit)
+    {
+        $formData->setNphParticipantId($participantId);
+        $formData->setNphModule($module);
+        $formData->setNphTimepoint($visit);
+        $this->em->persist($formData);
+        $this->em->flush();
+    }
+
     private function generateOrderSummaryArray(array $nphOrder): array
     {
         $sampleCount = 0;
