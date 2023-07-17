d9d4961110c8d0953bfdd2fbcb1b9b19261033f9
Shyam <shyam.s.naru@vumc.org>
2023-07-17 15:05:07 -0500

    Fix DSMT timepoint select all samples

--- web/assets/js/views/NphOrderCreate.js
+++ web/assets/js/views/NphOrderCreate.js
@@ -109,7 +109,7 @@ $(document).ready(function () {
 
     disableEnableStoolFields();
 
-    $(".stool-checkbox, #timepoint_preLMT, #nph_order_checkAll").on("change", disableEnableStoolFields);
+    $(".stool-checkbox, #timepoint_preLMT, #timepoint_preDSMT, #nph_order_checkAll").on("change", disableEnableStoolFields);
 
     if (
         $(".timepoint-samples input:checkbox").length === $(".timepoint-samples input:checkbox:disabled:checked").length
