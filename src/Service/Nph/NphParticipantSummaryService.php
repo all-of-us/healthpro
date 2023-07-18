69112b26c4967f3804e0b1cdcc285aced7743b9d
Shyam <shyam.s.naru@vumc.org>
2023-07-18 10:32:51 -0500

    Include nphBiospecimens field in the admin nph participant lookup

--- src/Service/Nph/NphParticipantSummaryService.php
+++ src/Service/Nph/NphParticipantSummaryService.php
@@ -302,6 +302,27 @@ class NphParticipantSummaryService
                                     status
                                     current
                                 }
+                            },
+                            nphBiospecimens {
+                                orderID
+                                visitID
+                                studyID
+                                specimenCode
+                                timepointID
+                                volume
+                                volumeUOM
+                                orderedSampleStatus
+                                clientID
+                                collectionDateUTC
+                                processingDateUTC
+                                finalizedDateUTC
+                                sampleID
+                                kitID
+                                biobankStatus {
+                                    limsID
+                                    biobankModified
+                                    status
+                                }
                             }
                         }
                     }
