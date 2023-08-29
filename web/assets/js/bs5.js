b3160f9e73591c9b94f429f21c3efd8688dcd2b5
Shyam <shyam.s.naru@vumc.org>
2023-08-29 13:48:23 -0500

    Fix scrollspy issue

--- web/assets/js/bs5.js
+++ web/assets/js/bs5.js
@@ -39,4 +39,12 @@ $(document).ready(function () {
         }
         new tempusDominus.TempusDominus(element, options);
     };
+
+    document.querySelectorAll('[data-bs-toggle="tab"]').forEach(el => {
+        el.addEventListener('shown.bs.tab', () => {
+            const target = el.getAttribute('data-bs-target')
+            const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+            bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+        });
+    });
 });
