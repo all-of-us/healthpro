79fa72fd7cbf47c6be5daf5597c12e1d9cc1fee1
Shyam <shyam.s.naru@vumc.org>
2023-09-06 17:00:16 -0500

    Create HeadCircumferenceForAge0To36Months entity and migration

---
+++ src/Entity/HeadCircumferenceForAge0To36Months.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HeadCircumferenceForAge0To36MonthsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HeadCircumferenceForAge0To36MonthsRepository::class)]
+class HeadCircumferenceForAge0To36Months
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $sex = null;
+
+    #[ORM\Column]
+    private ?float $month = null;
+
+    #[ORM\Column]
+    private ?float $L = null;
+
+    #[ORM\Column]
+    private ?float $M = null;
+
+    #[ORM\Column]
+    private ?float $S = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSex(): ?int
+    {
+        return $this->sex;
+    }
+
+    public function setSex(int $sex): self
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    public function getMonth(): ?float
+    {
+        return $this->month;
+    }
+
+    public function setMonth(float $month): self
+    {
+        $this->month = $month;
+
+        return $this;
+    }
+
+    public function getL(): ?float
+    {
+        return $this->L;
+    }
+
+    public function setL(float $L): self
+    {
+        $this->L = $L;
+
+        return $this;
+    }
+
+    public function getM(): ?float
+    {
+        return $this->M;
+    }
+
+    public function setM(float $M): self
+    {
+        $this->M = $M;
+
+        return $this;
+    }
+
+    public function getS(): ?float
+    {
+        return $this->S;
+    }
+
+    public function setS(float $S): self
+    {
+        $this->S = $S;
+
+        return $this;
+    }
+}
