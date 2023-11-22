67d087cb4fd9a4dd641eedf5e38247307f14ff44
Andrew Johnson <andrew.c.johnson@vumc.org>
2023-11-22 11:25:42 -0600

    Removed auto generated comments.

--- migrations/Version20231120043125.php
+++ migrations/Version20231120043125.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20231120043125 extends AbstractMigration
 {
     public function getDescription() : string
@@ -19,7 +16,6 @@ final class Version20231120043125 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('ALTER TABLE nph_orders ADD downtime_generated_user_id INT DEFAULT NULL, ADD downtime_generated TINYINT(1) NOT NULL, ADD downtime_generated_ts DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE nph_orders ADD CONSTRAINT FK_5E79E17DA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
@@ -30,9 +26,7 @@ final class Version20231120043125 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE nph_orders DROP FOREIGN KEY FK_5E79E17DA76ED395');
         $this->addSql('ALTER TABLE nph_orders DROP FOREIGN KEY FK_5E79E17D8B333355');
         $this->addSql('DROP INDEX IDX_5E79E17DA76ED395 ON nph_orders');
