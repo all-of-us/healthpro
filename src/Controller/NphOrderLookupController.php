5f49c978385a462a33689700325530140b5c0edd
Shyam <shyam.s.naru@vumc.org>
2023-12-06 14:17:30 -0600

    Add sample id lookup to biospecimen lookup page

--- src/Controller/NphOrderLookupController.php
+++ src/Controller/NphOrderLookupController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\NphOrder;
+use App\Entity\NphSample;
+use App\Form\Nph\NphSampleLookupType;
 use App\Form\OrderLookupIdType;
 use App\Service\Nph\NphParticipantSummaryService;
 use App\Service\SiteService;
@@ -37,7 +39,16 @@ class NphOrderLookupController extends AbstractController
         if ($idForm instanceof RedirectResponse) {
             return $idForm;
         }
-        return $this->generateOrderLookupView('program/nph/order/orderlookup.html.twig', $idForm, $recentOrders);
+        $sampleIdForm = $this->getSampleIdForm($request, $siteService, $participantSummary);
+        if ($sampleIdForm instanceof RedirectResponse) {
+            return $sampleIdForm;
+        }
+        return $this->generateOrderLookupView(
+            'program/nph/order/orderlookup.html.twig',
+            $idForm,
+            $sampleIdForm,
+            $recentOrders
+        );
     }
 
     private function getIdForm(
@@ -72,10 +83,50 @@ class NphOrderLookupController extends AbstractController
         }
         return $idForm;
     }
-    private function generateOrderLookupView(string $formpath, FormInterface $idForm, array $recentOrders = null): Response
-    {
+
+    private function getSampleIdForm(
+        Request $request,
+        SiteService $siteService,
+        NphParticipantSummaryService $nphParticipantSummaryService
+    ): FormInterface|RedirectResponse {
+        $sampleIdForm = $this->createForm(NphSampleLookupType::class, null);
+        $sampleIdForm->handleRequest($request);
+
+        if ($sampleIdForm->isSubmitted() && $sampleIdForm->isValid()) {
+            $id = $sampleIdForm->get('sampleId')->getData();
+
+            $sample = $this->em->getRepository(NphSample::class)->findOneBy([
+                'sampleId' => $id
+            ]);
+            if ($sample) {
+                $participantId = $sample->getNphOrder()->getParticipantId();
+                $participant = $nphParticipantSummaryService->getParticipantById($participantId);
+                if (!$participant) {
+                    throw $this->createNotFoundException('Participant not found.');
+                }
+                if ($participant->nphPairedSiteSuffix === $siteService->getSiteId()) {
+                    return $this->redirectToRoute('nph_sample_finalize', [
+                        'participantId' => $sample->getNphOrder()->getParticipantId(),
+                        'orderId' => $sample->getNphOrder()->getId(),
+                        'sampleId' => $sample->getId()
+                    ]);
+                }
+                $crossSiteErrorMessage = 'Lookup for this sample ID is not permitted because the participant is paired with another site';
+            }
+            $this->addFlash('error', $crossSiteErrorMessage ?? 'Sample ID not found');
+        }
+        return $sampleIdForm;
+    }
+
+    private function generateOrderLookupView(
+        string $formpath,
+        FormInterface $idForm,
+        FormInterface $sampleIdForm,
+        array $recentOrders = null
+    ): Response {
         return $this->render($formpath, [
             'idForm' => $idForm->createView(),
+            'sampleIdForm' => $sampleIdForm->createView(),
             'recentOrders' => $recentOrders
         ]);
     }
