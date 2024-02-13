cb6300a2078e4162f2795703a9259c771a7b2f2d
Andrew Johnson <andrew.c.johnson@vumc.org>
2024-02-13 12:03:38 -0600

    Check if property set before accessing.

--- src/Helper/Participant.php
+++ src/Helper/Participant.php
@@ -515,20 +515,22 @@ class Participant
             $this->pediatricMeasurementsVersionType = $measurementVersionType;
         }
 
-        if ($participant->enrollmentStatusV3_2 === 'PARTICIPANT_PLUS_EHR') {
-            $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusParticipantPlusEhrV3_2Time;
-        } elseif ($participant->enrollmentStatusV3_2 === 'ENROLLED_PARTICIPANT') {
-            $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusEnrolledParticipantV3_2Time;
-        } elseif ($participant->enrollmentStatusV3_2 === 'PARTICIPANT') {
-            $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusParticipantV3_2Time;
-        } elseif ($participant->enrollmentStatusV3_2 === 'PMB_ELIGIBLE') {
-            $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusPmbEligibleV3_2Time;
-        } elseif ($participant->enrollmentStatusV3_2 === 'CORE_PARTICIPANT') {
-            $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusCoreV3_2Time;
-        } elseif ($participant->enrollmentStatusV3_2 === 'CORE_MINUS_PM') {
-            $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusCoreMinusPmV3_2Time;
-        } else {
-            $this->enrollmentStatusV3_2Time = null;
+        if (isset($participant->enrollmentStatusV3_2)) {
+            if ($participant->enrollmentStatusV3_2 === 'PARTICIPANT_PLUS_EHR') {
+                $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusParticipantPlusEhrV3_2Time;
+            } elseif ($participant->enrollmentStatusV3_2 === 'ENROLLED_PARTICIPANT') {
+                $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusEnrolledParticipantV3_2Time;
+            } elseif ($participant->enrollmentStatusV3_2 === 'PARTICIPANT') {
+                $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusParticipantV3_2Time;
+            } elseif ($participant->enrollmentStatusV3_2 === 'PMB_ELIGIBLE') {
+                $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusPmbEligibleV3_2Time;
+            } elseif ($participant->enrollmentStatusV3_2 === 'CORE_PARTICIPANT') {
+                $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusCoreV3_2Time;
+            } elseif ($participant->enrollmentStatusV3_2 === 'CORE_MINUS_PM') {
+                $this->enrollmentStatusV3_2Time = $participant->enrollmentStatusCoreMinusPmV3_2Time;
+            } else {
+                $this->enrollmentStatusV3_2Time = null;
+            }
         }
     }
 
