b092a6d5814c797041fe98e1d960e979023fdc45
Shyam <shyam.s.naru@vumc.org>
2024-02-14 15:50:28 -0600

    Remove scale and implement tenth place validation for Actual Dose

--- src/Form/DlwType.php
+++ src/Form/DlwType.php
@@ -17,6 +17,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 class DlwType extends AbstractType
 {
     private const DOSE_BATCH_ID_DIGITS = 8;
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -33,36 +34,43 @@ class DlwType extends AbstractType
             ->add('actualDose', NumberType::class, [
                 'label' => 'Actual Dose (g)*',
                 'required' => true,
-                'scale' => 1,
                 'empty_data' => 0,
-                'constraints' => new GreaterThan([
-                    'value' => 0,
-                    'message' => 'Dose must be greater than 0.'
-                ])
+                'constraints' => [
+                    new GreaterThan([
+                        'value' => 0,
+                        'message' => 'Dose must be greater than 0.'
+                    ]),
+                    new Callback(function ($value, $context) {
+                        if ($this->getNumDecimalPlaces($value) > 1) {
+                            $context->buildViolation('Please verify the measurement is correct. Value can be entered up to the tenths (0.1) place.')
+                                ->addViolation();
+                        }
+                    })
+                ]
             ])
             ->add('participantWeight', NumberType::class, [
                 'required' => true,
                 'label' => 'Participant Weight (kg)*',
                 'empty_data' => 0,
-                'constraints' => new Callback(function ($value, $context) {
-                    if ($value < 0 || $value === null) {
-                        $context->buildViolation('Participant weight required.')
-                            ->addViolation();
-                    }
-                    if ($value == 0) {
-                        $context->buildViolation('Please verify the measurement is correct. Value should be greater than 0 kg.')
-                            ->addViolation();
-                    }
-                    if ($value > 907) {
-                        $context->buildViolation('Please verify the measurement is correct. Value should be less than 907 kg.')
-                            ->addViolation();
-                    }
-                    if ($this->getNumDecimalPlaces($value) > 1) {
-                        $context->buildViolation('Please verify the measurement is correct. Value can be entered up to the tenths (0.1) place.')
-                            ->addViolation();
-                    }
-                }),
-                'scale' => 1,
+                'constraints' => [
+                    new GreaterThan([
+                        'value' => 0,
+                        'message' => 'Please verify the measurement is correct. Value should be greater than 0 kg.'
+                    ]),
+                    new Callback(function ($value, $context) {
+                        if ($value < 0 || $value === null) {
+                            $context->buildViolation('Participant weight required.')
+                                ->addViolation();
+                        }
+                        if ($value > 907) {
+                            $context->buildViolation('Please verify the measurement is correct. Value should be less than 907 kg.')
+                                ->addViolation();
+                        }
+                        if ($this->getNumDecimalPlaces($value) > 1) {
+                            $context->buildViolation('Please verify the measurement is correct. Value can be entered up to the tenths (0.1) place.')
+                                ->addViolation();
+                        }
+                    })]
             ])
             ->add('doseAdministered', DateTimeType::class, [
                 'format' => 'M/d/yyyy h:mm a',
@@ -75,8 +83,7 @@ class DlwType extends AbstractType
                 'view_timezone' => $options['timezone'],
                 'constraints' => new NotBlank(['message' => 'Dose date/time required.'])
             ])
-            ->add('calculatedDose', null, ['attr' => ['readonly' => true], 'mapped' => false])
-        ;
+            ->add('calculatedDose', null, ['attr' => ['readonly' => true], 'mapped' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -90,7 +97,7 @@ class DlwType extends AbstractType
 
     private function getNumDecimalPlaces($num): int
     {
-        if ((int) $num == $num) {
+        if ((int)$num == $num) {
             return 0;
         } elseif (!is_numeric($num)) {
             return 0;
