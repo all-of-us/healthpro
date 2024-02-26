fd0748a6dc52f0ee075977d0e7de83c2826b0956
Andrew Johnson <andrew.c.johnson@vumc.org>
2024-02-26 08:59:22 -0600

    Added missing debug function

--- src/WorkQueue/DataSources/WorkqueueDatasource.php
+++ src/WorkQueue/DataSources/WorkqueueDatasource.php
@@ -8,4 +8,6 @@ interface WorkqueueDatasource
 {
     public function getWorkqueueData(int $offset, int $limit, ColumnCollection $columnCollection): array;
     public function hasMoreResults(): bool;
+    //Todo: Remove before production merge.
+    public function rawQuery($query);
 }
