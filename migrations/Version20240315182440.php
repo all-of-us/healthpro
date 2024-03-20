12692fbd943e4729cd0f9d3f985be4740cee37a4
Andrew Johnson <andrew.c.johnson@vumc.org>
2024-03-20 09:08:19 -0500

    Added regex check and removed auto generated comments.

--- migrations/Version20240315182440.php
+++ migrations/Version20240315182440.php
@@ -7,24 +7,21 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240315182440 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return 'Adds other recipient field to incentive table to track incentives given to related participants.';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE incentive ADD related_participant_recipient VARCHAR(50) DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
