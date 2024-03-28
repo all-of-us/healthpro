2bbbb327c9590e187b532786fd732c38d7828dd1
Shyam <shyam.s.naru@vumc.org>
2024-03-28 15:36:35 -0500

    Fix unit tests

--- tests/Service/WorkQueueServiceTest.php
+++ tests/Service/WorkQueueServiceTest.php
@@ -297,7 +297,11 @@ class WorkQueueServiceTest extends ServiceTestCase
             '',
             '',
             'Adult Participant',
-            'N/A'
+            'N/A',
+            '0',
+            '',
+            '0',
+            ''
         ], $row);
     }
 
