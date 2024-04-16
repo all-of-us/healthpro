047553b5cabb38f4489d45766e3695379637694a
Andrew Johnson <andrew.c.johnson@vumc.org>
2024-04-16 11:21:29 -0500

    Remove auto generated comments.

--- migrations/Version20240416023515.php
+++ migrations/Version20240416023515.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240416023515 extends AbstractMigration
 {
     public function getDescription() : string
@@ -26,7 +23,6 @@ final class Version20240416023515 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE orders_history DROP samples_version');
