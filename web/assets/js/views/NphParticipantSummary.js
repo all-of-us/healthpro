de4c80e2c69c13cbec94c12968bbc922b86dbdd0
Shyam <shyam.s.naru@vumc.org>
2024-04-26 16:03:58 -0500

    Fix JS code style

--- web/assets/js/views/NphParticipantSummary.js
+++ web/assets/js/views/NphParticipantSummary.js
@@ -26,17 +26,20 @@ $(document).ready(function () {
         $("#nph_sample_process_complete_period").val(visitType);
         if ($(this).is(":checked")) {
             processingComplete = 1;
-            modelBodyText = "<p>Are you sure you want mark the samples as processing complete?</p>"
-            if (dietStatus === 'in_progress_unfinalized') {
+            modelBodyText = "<p>Are you sure you want mark the samples as processing complete?</p>";
+            if (dietStatus === "in_progress_unfinalized") {
                 modelTitleText = "<span class='text-danger'>Warning!</span>";
-                let moduleId = parseInt(moduleNumber) === 1 ? 'sample_process_complete_unfinalized_message_1' : "sample_process_complete_unfinalized_message";
+                let moduleId =
+                    parseInt(moduleNumber) === 1
+                        ? "sample_process_complete_unfinalized_message_1"
+                        : "sample_process_complete_unfinalized_message";
                 modelBodyText = $("#" + moduleId).html();
             }
         }
         $("#nph_sample_process_complete_status").val(processingComplete);
         let modelSel = $("#sample_process_complete_modal");
-        modelSel.find('.modal-title').html(modelTitleText)
-        modelSel.find('.modal-body').html(modelBodyText)
+        modelSel.find(".modal-title").html(modelTitleText);
+        modelSel.find(".modal-body").html(modelBodyText);
         let modal = new bootstrap.Modal(modelSel);
         modal.show();
     });
@@ -48,8 +51,8 @@ $(document).ready(function () {
     $("#sample_process_go_back").on("click", function () {
         let moduleNumber = $(".nav-item.participant-module.active").data("modulenumber");
         let moduleSel = $("#ModuleGroup" + moduleNumber);
-        let period = moduleSel.find(".nav-link.participant-diet-period.active").data('period');
+        let period = moduleSel.find(".nav-link.participant-diet-period.active").data("period");
         let checkBoxSel = moduleSel.find("#sample_process_complete_check_" + period);
-        checkBoxSel.prop('checked', !checkBoxSel.prop('checked'));
+        checkBoxSel.prop("checked", !checkBoxSel.prop("checked"));
     });
 });
