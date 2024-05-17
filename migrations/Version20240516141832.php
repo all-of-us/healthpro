1c6b17884039fe8a324dcca23c354e585f14dc5a
Andrew Johnson <andrew.c.johnson@vumc.org>
2024-05-17 09:57:48 -0500

    Remove generated comment

--- migrations/Version20240516141832.php
+++ migrations/Version20240516141832.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240516141832 extends AbstractMigration
 {
     public function getDescription() : string
