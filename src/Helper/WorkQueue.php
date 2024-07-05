a3a7be450cbfde65d3070af0d8b3e0f021991bcb
Shyam Naru <shyamnaru@gmail.com>
2024-07-05 10:16:22 -0500

    Revert "Revert "[HPRO-1859] Add new PST samples to WQ and Participant Summary Pages""

--- src/Helper/WorkQueue.php
+++ src/Helper/WorkQueue.php
@@ -1141,6 +1141,38 @@ class WorkQueue
             'group' => 'enrollment',
             'display_na' => self::NA_PEDIATRIC
         ],
+        'PS04A' => [
+            'name' => '1st 4.5 mL PST',
+            'csvNames' => [
+                '1st 4.5 mL PST Received',
+                '1st 4.5 mL PST Received Date'
+            ],
+            'rdrField' => 'sampleStatus1PS4A',
+            'sortField' => 'sampleStatus1PS4ATime',
+            'rdrDateField' => 'sampleStatus1PS4ATime',
+            'htmlClass' => 'text-center',
+            'toggleColumn' => true,
+            'type' => 'sample',
+            'visible' => false,
+            'group' => 'enrollment',
+            'display_na' => self::NA_PEDIATRIC
+        ],
+        'PS04B' => [
+            'name' => '2nd 4.5 mL PST',
+            'csvNames' => [
+                '2nd 4.5 mL PST Received',
+                '2nd 4.5 mL PST Received Date'
+            ],
+            'rdrField' => 'sampleStatus1PS4B',
+            'sortField' => 'sampleStatus1PS4BTime',
+            'rdrDateField' => 'sampleStatus1PS4BTime',
+            'htmlClass' => 'text-center',
+            'toggleColumn' => true,
+            'type' => 'sample',
+            'visible' => false,
+            'group' => 'enrollment',
+            'display_na' => self::NA_PEDIATRIC
+        ],
         '1HEP4' => [
             'name' => '4 mL Na-Hep',
             'csvNames' => [
@@ -1509,6 +1541,8 @@ class WorkQueue
         'orderCreatedSite',
         '1SST8',
         '1PST8',
+        'PS04A',
+        'PS04B',
         '1HEP4',
         '1ED02',
         '2ED02',
@@ -1696,7 +1730,9 @@ class WorkQueue
         'enrollmentStatusParticipantPlusEhrV3_2Time',
         'enrollmentStatusPmbEligibleV3_2Time',
         'pediatricStatus',
-        'relatedParticipants'
+        'relatedParticipants',
+        'PS04A',
+        'PS04B'
     ];
 
     public static $sortColumns = [
@@ -1780,6 +1816,8 @@ class WorkQueue
         'biospecimenSourceSite',
         'sampleStatus1SST8Time',
         'sampleStatus1PST8Time',
+        'sampleStatus1PS4ATime',
+        'sampleStatus1PS4BTime',
         'sampleStatus1HEP4Time',
         'sampleStatus1ED02Time',
         'sampleStatus2ED02Time',
@@ -2351,6 +2389,22 @@ class WorkQueue
                     'Not Received' => 'UNSET'
                 ]
             ],
+            'sampleStatus1PS4A' => [
+                'label' => '1st 4.5 mL PST',
+                'options' => [
+                    'View All' => '',
+                    'Received' => 'RECEIVED',
+                    'Not Received' => 'UNSET'
+                ]
+            ],
+            'sampleStatus1PS4B' => [
+                'label' => '2nd 4.5 mL PST',
+                'options' => [
+                    'View All' => '',
+                    'Received' => 'RECEIVED',
+                    'Not Received' => 'UNSET'
+                ]
+            ],
             'sampleStatus1HEP4' => [
                 'label' => '4 mL Na-Hep',
                 'options' => [
@@ -2465,6 +2519,8 @@ class WorkQueue
         'clinicPhysicalMeasurementsStatus',
         'sampleStatus1SST8',
         'sampleStatus1PST8',
+        'sampleStatus1PS4A',
+        'sampleStatus1PS4B',
         'sampleStatus1HEP4',
         'sampleStatus1ED02',
         'sampleStatus2ED02',
@@ -2605,6 +2661,8 @@ class WorkQueue
     public static $samples = [
         '1SST8' => '8 mL SST',
         '1PST8' => '8 mL PST',
+        'PS04A' => '1st 4.5 mL PST',
+        'PS04B' => '2nd 4.5 mL PST',
         '1HEP4' => '4 mL Na-Hep',
         '1ED02' => '2 mL EDTA (1ED02)',
         '2ED02' => '2 mL EDTA (2ED02)',
@@ -2639,11 +2697,15 @@ class WorkQueue
     public static $samplesAlias = [
         [
             '1SST8' => '1SS08',
-            '1PST8' => '1PS08'
+            '1PST8' => '1PS08',
+            'PS04A' => '1PS4A',
+            'PS04B' => '1PS4B',
         ],
         [
             '1SST8' => '2SST8',
-            '1PST8' => '2PST8'
+            '1PST8' => '2PST8',
+            'PS04A' => '2PS4A',
+            'PS04B' => '2PS4B',
         ],
         [
             '1SAL' => '1SAL2'
