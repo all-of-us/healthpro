229b00f3512ba974d6f8fa7e7ca9f760abe5ed2e
Shyam <shyam.s.naru@vumc.org>
2024-10-23 16:29:41 -0500

    Fix code style

--- src/Form/SiteType.php
+++ src/Form/SiteType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
-use Symfony\Component\Validator\Validation;
 
 class SiteType extends AbstractType
 {
