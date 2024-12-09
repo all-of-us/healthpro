2046f51b3c9a076bb0fc22375c5f6561ba714165
Shyam <shyam.s.naru@vumc.org>
2024-12-09 10:10:39 -0600

    Fix biobank view check all

--- web/assets/js/views/BiobankOrder.js
+++ web/assets/js/views/BiobankOrder.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var currentStep = JSON.parse($("#currentStep").data("current-step"));
+    var currentStep = $("#currentStep").data("current-step");
     if ($(".finalize-form .alert-danger").length !== 0) {
         currentStep = "finalize";
     }
@@ -15,10 +15,10 @@ $(document).ready(function () {
     $('.finalize-form button[type="submit"]').on("click", function () {
         //Display warning message
         var message = "Are you sure you want to finalize this order?";
-        var collectedSamples = JSON.parse($("#collectedSamples").data("collected-samples"));
-        $('input[name="form[finalized_samples][]"]').each(function () {
+        var collectedSamples = $("#collectedSamples").data("collected-samples");
+        $('input[name="biobank_order[finalizedSamples][]"]').each(function () {
             //Select samples that are unchecked and not disabled
-            if ($(this).prop("checked") === false && collectedSamples && $.inArray($(this).val(), collectedSamples)) {
+            if ($(this).prop("checked") === false && collectedSamples && $.inArray($(this).val(), collectedSamples) !== -1) {
                 message =
                     "Warning: At least one sample that was collected and processed (as applicable) was not finalized. Are you sure you wish to continue?";
                 return false;
