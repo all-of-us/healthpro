da31f66f75acb0e977e252c68591471e10fb287d
Shyam <shyam.s.naru@vumc.org>
2025-05-21 11:53:50 -0500

    Update Biobank order details view to bootstrap 5

--- web/assets/js/views/BiobankOrder.js
+++ web/assets/js/views/BiobankOrder.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
         currentStep = "finalize";
     }
     // Switch tab to active step
-    $(".nav-tabs a[href='#" + currentStep + "']").tab("show");
+    let targetSelector = ".nav-tabs button[data-bs-target='#" + currentStep + "']";
+    let tabTriggerEl = $(targetSelector)[0];
+    if (tabTriggerEl) {
+        let tab = new bootstrap.Tab(tabTriggerEl);
+        tab.show();
+    }
 
     $("#checkall").on("change", function () {
         $("#biobank_order_finalizedSamples input:checkbox:enabled").prop("checked", $(this).prop("checked"));
