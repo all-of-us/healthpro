bf33b69cb40b8a0a08077f190ee141c7a8b28fd4
Shyam <shyam.s.naru@vumc.org>
2025-06-18 15:57:00 -0500

    Group sample by diet period in cancel all page

--- web/assets/js/views/NphOrderModify.js
+++ web/assets/js/views/NphOrderModify.js
@@ -13,12 +13,17 @@ $(document).ready(function () {
         showHideOtherField();
     });
 
-    $("#modify_check_all").on("change", function () {
-        $(".modify-samples input:checkbox:enabled").prop("checked", $(this).prop("checked"));
+    $(document).on("change", ".modify-check-all", function () {
+        const $accordion = $(this).closest(".accordion-item");
+        const isChecked = $(this).is(":checked");
+        $accordion.find(".modify-samples input[type='checkbox']:enabled").prop("checked", isChecked);
     });
 
-    $("input:checkbox").on("change", function () {
-        let allSamplesChecked = $(".modify-samples :checkbox:not(:checked)").length === 0;
-        $("#modify_check_all").prop("checked", allSamplesChecked);
+    $(document).on("change", ".modify-samples input[type='checkbox']", function () {
+        const $accordionItem = $(this).closest(".accordion-item");
+        const $checkAll = $accordionItem.find(".modify-check-all");
+        const total = $accordionItem.find(".modify-samples input[type='checkbox']:enabled").length;
+        const checked = $accordionItem.find(".modify-samples input[type='checkbox']:enabled:checked").length;
+        $checkAll.prop("checked", total > 0 && total === checked);
     });
 });
