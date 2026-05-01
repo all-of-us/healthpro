5ce5dcc48ab55375337b04faa98f5ff349e791eb
Shyam <shyam.s.naru@vumc.org>
2026-05-01 15:14:57 -0500

    Update pediatric assent response handling

--- web/assets/js/views/PediatricAssent.js
+++ web/assets/js/views/PediatricAssent.js
@@ -2,15 +2,23 @@ $(document).ready(function () {
     const $assentSelect = $("#pediatricAssent");
     const $continueBtn = $("#continueBtn");
     const $errorMessage = $("#assentErrorMessage");
+    const getValues = (dataKey) => (($assentSelect.data(dataKey) || "").toString()
+        .split(",")
+        .map((value) => value.trim())
+        .filter(Boolean));
+    const continueValues = getValues("continueValues");
+    const errorValues = getValues("errorValues");
     const resetState = () => {
         $continueBtn.addClass("d-none");
         $errorMessage.addClass("d-none");
     };
     resetState();
     $assentSelect.on("change", () => {
-        const value = $assentSelect.val();
+        const value = ($assentSelect.val() || "").toString();
         resetState();
-        if (value === "no") $errorMessage.removeClass("d-none");
-        if (value === "yes") $continueBtn.removeClass("d-none");
+        if (continueValues.includes(value)) {
+            $continueBtn.removeClass("d-none");
+        }
+        if (errorValues.includes(value)) $errorMessage.removeClass("d-none");
     });
 });
